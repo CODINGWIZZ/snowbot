3b6ee3d62e273425d8b740b4ebd503333452a5d6
WizzCoding <33423684+WizzCoding@users.noreply.github.com>
2018-06-02 23:59:02 +0200

    Update forecast.js

--- commands/forecast.js
+++ commands/forecast.js
@@ -26,6 +26,12 @@ module.exports.run = async (bot, message, args) => {
                 Sunday: "SUNDAY"
 
             };
+            
+            const weatherInfo = res.body.query.results.channel;
+            const forecast = weatherInfo.item.forecast[0];
+
+            weather.find({search: args.join(" "), degreeType: "F"}, function(err, result) {
+                //if (err) message.channel.send(err);
 
             let makeURL1 = (city) => `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22${encodeURIComponent(city)}%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
             const res = await got(makeURL1(args.join(" ")), { json: true });
@@ -34,13 +40,7 @@ module.exports.run = async (bot, message, args) => {
 
             if (!res || !res.body || !res.body.query || !res.body.query.results || !res.body.query.results.channel) {
                 return message.channel.send("COULDN'T CHECK FORECAST**!**");
-              }
-
-            const weatherInfo = res.body.query.results.channel;
-            const forecast = weatherInfo.item.forecast[0];
-
-            weather.find({search: args.join(" "), degreeType: "F"}, function(err, result) {
-                //if (err) message.channel.send(err);
+            }
 
             const city = args.join(" ");
             if(!city) return message.channel.send("PLEASE ENTER A CITY YOU WANT TO CHECK FORECAST FOR**!**");
