c621e6b6809c3a02c10736f96b3bd6066a6aa0e1
WizzCoding <33423684+WizzCoding@users.noreply.github.com>
2018-06-04 08:05:12 +0200

    Create mute.js

---
+++ commands/mute.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const ms = require("ms");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0].toLocaleLowerCase();
+ 
+
+}
+
+module.exports.help {
+  name: "mute"
+}
