a83cd621caca6a1a954f0365521ba167c3b28b65
WizzCoding <33423684+WizzCoding@users.noreply.github.com>
2018-06-11 15:32:06 +0200

    Update cat.js

--- commands/cat.js
+++ commands/cat.js
@@ -7,6 +7,25 @@ module.exports.run = async (bot, message, args) => {
   let prefix = botconfig.prefix;
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0].toLocaleLowerCase();
+  
+  // CAT COMMAND
+  if(cmd === `${prefix}cat`) {
+        
+  message.channel.send("GENERATING CAT IMAGE **...**").then((catImage) => {
+
+  const { body, header } = await superagent
+  .get(`http://aws.random.cat//meow`);
+    
+  const catEmbed = new Discord.RichEmbed()
+  .setColor(botconfig.blue)
+  .setDescription("**CAT ❆**")
+  .setImage(body.file);
+
+  catImage.edit(catEmbed);
+        
+  });
+
+  }
 
 }
 
