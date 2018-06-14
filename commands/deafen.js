6e927c1e03b5526d6d12c0fd2100a7a93594dd2e
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-14 15:59:47 +0200

    Update deafen.js

--- commands/deafen.js
+++ commands/deafen.js
@@ -49,6 +49,10 @@ module.exports.run = async (bot, message, args) => {
 
     let mutetime = args[1];
     if(!mutetime) return message.channel.send("SPECIFY A TIME**!**");
+      
+    let mutetimeimp = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "M", "S", "H", "DAYS"];
+        
+    if(!mutetimeimp.includes(mutetime)) return message.channel.send("PLEASE ENTER A SPECIFIC TIME YOU WANT THE DESIRED USER TO BE DEAFENED FOR**!**");
 
     await(dUser.addRole(muterole.id));
     message.channel.send(`<@${dUser.id}> HAS BEEN **DEAFENED** FOR **${ms(ms(mutetime))}!**`);
