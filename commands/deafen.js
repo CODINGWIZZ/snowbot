0696275de7467a438bb7f3a3c512f13fca1a66b6
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-20 23:52:52 +0200

    Update deafen.js

--- commands/deafen.js
+++ commands/deafen.js
@@ -51,7 +51,7 @@ module.exports.run = async (bot, message, args) => {
     if(!mutetime) return message.channel.send("SPECIFY A TIME**!**");
     
     await(dUser.addRole(deafenrole.id));
-    message.channel.send(`<@${dUser.id}> HAS BEEN **DEAFENED** FOR **${ms(ms(mutetime))}!**`);
+    message.channel.send(`<:SNOWCHECK:459111379899514887> **//** <@${dUser.id}> HAS BEEN **DEAFENED** FOR **${ms(ms(mutetime))}!**`);
 
     let muteEmbed = new Discord.RichEmbed()
     .setColor(botconfig.blue)
