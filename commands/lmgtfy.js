21f3031e6e575cf7f6736d8644959bb53194860a
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-20 17:53:16 +0200

    Update lmgtfy.js

--- commands/lmgtfy.js
+++ commands/lmgtfy.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
-const botconfig = require("../botconfig.json");
 const encode = require("strict-encode-uri");
+const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
 
