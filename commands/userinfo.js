a93cdc4a39b63a48108b57b82b3506a679c663be
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-20 14:17:35 +0200

    Create userinfo.js

---
+++ commands/userinfo.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0].toLocaleLowerCase();
+  
+  // USER INFO
+  if(cmd === `${prefix}userinfo`) {
+
+  let user = message.mentions.users.first() || message.guild.members.get(args [0]) || message.author;
+  
+  let userinfoEmbed = new Discord.RichEmbed()
+  .setColor(botconfig.blue)
+  .setDescription("**" + user.username + "**")
+  .setAuthor("USER INFO ❆")
+  .addField("FULL NAME", `**${user.username}**#${user.discriminator}`)
+  .addField("ID", user.id)
+  // .addField("JOINED THIS SERVER", user.guild.joinedAt)
+  .addField("CREATED", user.createdAt.toDateString())
+  .setFooter("USER INFO | SNOW ❆", user.displayAvatarURL);
+
+  message.channel.send(userinfoEmbed);
+  
+    }
+
+}
+
+module.exports.help = {
+  name: "userinfo"
+}
