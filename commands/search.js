9af591494bd2955069385811b944c7abf3e612d5
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-21 00:17:22 +0200

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -9,10 +9,8 @@ module.exports.run = async (bot, message, args) => {
   let cmd = messageArray[0].toLocaleLowerCase();
   
   // GOOGLE | SEARCH COMMAND
-  if(cmd === `${prefix}search`) {
+  if(cmd === `${prefix}google` || cmd === `${prefix}search`) {
                                                               
-  if(cmd === `${prefix}google`) {                                          
-        
   let google = encode(args.join(" "));
   if(!google) return message.channel.send("PLEASE ENTER A SEARCH QUERY**!**");
         
@@ -23,13 +21,11 @@ module.exports.run = async (bot, message, args) => {
   return googleMessage.edit(`<:SNOWCHECK:459111379899514887> **//** **FINISHED!**\n<${googleLink}>`);
             
     }); 
-    
-  }
         
   }
 
 }
 
 module.exports.help = {
-  name: "search"
+  name: "search", "google"
 }
