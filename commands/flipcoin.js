46db4e805b1be5ddfb4b5212738805298f4b49b9
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-20 14:35:01 +0200

    Create flipcoin.js

---
+++ commands/flipcoin.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0].toLocaleLowerCase();
+  
+  // FLIP A COIN
+  if(cmd === `${prefix}flipcoin` || cmd === `${prefix}coin`) {
+
+  let coin = ["HEADS", "TAILS"];
+
+  let coinrandom = Math.floor((Math.random()) * coin.length);
+
+  message.channel.send("**" + message.author.username + ",** I FLIPPED **" + coin[coinrandom] + "!**");
+
+  }
+  
+}
+
+module.export.help = {
+  name: "flipcoin"
+}
