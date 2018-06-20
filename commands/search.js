a4b68b308202c42ccaddb04e203ab2b37c74ff92
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-20 17:52:14 +0200

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const encode = require("strict-uri-encode")
 const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
