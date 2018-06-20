028e60c2a7385b940fcbc8f562c1df15becab282
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-20 17:46:09 +0200

    Create randomcolor.js

---
+++ commands/randomcolor.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0].toLocaleLowerCase();
+  
+      // RANDOM COLOR COMMAND
+    if(cmd === `${prefix}randomcolor`) {
+
+    let color = "000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);}).toLocaleLowerCase();
+
+    let randomColorEmbed = new Discord.RichEmbed()
+    .setAuthor(`#${color.toUpperCase()}`, `https://dummyimage.com/250/${color}/&text=%20`)
+    .setTitle("[INFORMATION]")
+    .setColor(`${color}`)
+    .setURL(`https://www.colorhexa.com/${color}`)
+    .setDescription(`**RGB**\n${convert.hex.rgb(color)}\n\n**HSL**\n${convert.hex.hsl(color)}\n\n**CSS**\n${convert.hex.keyword(color).toUpperCase()}`)
+    .setFooter("RANDOM COLOR | SNOW ❆", bot.user.displayAvatarURL);
+
+    return message.channel.send(randomColorEmbed);
+
+    }
+
+}
+
+module.exports.run = {
+  name: "randomcolor"
+} 
