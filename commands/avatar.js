32677f49550ea2479cc499d098a9ac340ceff16f
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-20 18:15:30 +0200

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0].toLocaleLowerCase();
+  
+      // CHECK AVATAR
+    if(cmd === `${prefix}avatar`) {
+
+    const avatar = message.mentions.users.first() || message.author;
+    
+    if (!avatar.avatarURL) return message.channel.send("CAN'T FIND AVATAR**!**");
+
+    let avatarEmbed = new Discord.RichEmbed()
+    .setColor(botconfig.blue)
+    .setAuthor(avatar.username + "  /  AVATAR ❆")
+    .setTitle("[DOWNLOAD]")
+    .setURL(avatar.avatarURL)
+    .setImage(avatar.avatarURL)
+    .setFooter("AVATAR | SNOW ❆", bot.user.displayAvatarURL);
+
+    return message.channel.send(avatarEmbed);
+
+}
+
+
+
+
+  
+}
+
+module.exports.help = {
+  name: "avatar"
+} 
