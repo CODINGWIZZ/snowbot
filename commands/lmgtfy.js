3fbedd4a4675ed62e15af207caf2cf370c129d72
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-20 17:50:43 +0200

    Create lmgtfy.js

---
+++ commands/lmgtfy.js
@@ -0,0 +1,31 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+const encode = require("strict-encode-uri");
+
+module.exports.run = async (bot, message, args) => {
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0].toLocaleLowerCase();
+  
+      // LMGTYFY COMMAND
+    if(cmd === `${prefix}lmgtfy`) {
+
+        let question = encode(args.join(" "));
+        if(!question) return message.channel.send("PLEASE ENTER A QUESITON YOU WANT TO MAKE WITH LMGTFY**!**");
+
+        message.channel.send("GENERATING **...**").then((loadingMessage) => {
+
+        let link = `https://lmgtfy.com/?q=${question}`;
+            
+        return loadingMessage.edit("**FINISHED!**" + "\n" + `<${link}>`);
+     
+        });
+
+    }
+  
+}
+
+module.exports.help = {
+  name: "lmgtfy"
+}
