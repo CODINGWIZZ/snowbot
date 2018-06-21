b850af7f0838843a2687c1d9e79b3310fe2a777b
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-21 17:04:53 +0200

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -14,6 +14,7 @@ module.exports.run = async (bot, message, args) => {
     // if(!permissions.has("MANAGE_MESSAGES")) return message.channel.send("CANNOT DO THAT **-** MAKE SURE I HAVE THE RIGHT PERMISSIONS**!**");
     if(!args[0]) return message.channel.send("PLASE ENTER A MEMBER YOU WANT TO WARN**!**");
     let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO WARN**!**");
     if(!wUser) return message.channel.send("CAN'T FIND USER**!**");
     if(wUser.id === message.author.id) return message.channel.send("YOU CAN NOT WARN YOURSELF**!**");
     if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("THAT USER CAN'T BE WARNED**!**");
