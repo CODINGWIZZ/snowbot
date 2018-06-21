5ed1fbb35f7ab4622f98e823bcc456ee9ce8b26d
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-21 09:35:52 +0200

    Update calculate.js

--- commands/calculate.js
+++ commands/calculate.js
@@ -8,29 +8,7 @@ module.exports.run = async (bot, message, args) => {
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0].toLocaleLowerCase();
     
-    // CALCULATE
-    if(cmd === `${prefix}calculate` || cmd === `${prefix}calculator`) {
 
-    const question = args.join(" ");
-    if(!question) return message.channel.send("YOU MUST PROVIDE A EQUATION IF YOU WANT TO CACULATE**!**");
-
-    let answer = math.eval(question);
-      
-    try {
-    } catch (err) {
-        return message.channel.send("INVILID MATH EQUATION**!**");
-    }
-
-    let mathEmbed = new Discord.RichEmbed()
-    .setColor(botconfig.blue)
-    .setDescription("**CALCULATOR ❆**")
-    .addField("EQUATION", `\`\`\`${question}\`\`\``)
-    .addField("ANSWER", `\`\`\`${answer}\`\`\``)
-    .setFooter("CALCULATOR | SNOW ❆", bot.user.displayAvatarURL);
-
-    message.channel.send(mathEmbed);
-        
-  }
   
 }
 
