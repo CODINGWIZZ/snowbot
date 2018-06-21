82980749d9d0e781853e8c2489214c21615efe22
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-21 09:36:24 +0200

    Update flipcoin.js

--- commands/flipcoin.js
+++ commands/flipcoin.js
@@ -7,16 +7,6 @@ module.exports.run = async (bot, message, args) => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0].toLocaleLowerCase();
   
-  // FLIP A COIN
-  if(cmd === `${prefix}flipcoin` || cmd === `${prefix}coin`) {
-
-  let coin = ["HEADS", "TAILS"];
-
-  let coinrandom = Math.floor((Math.random()) * coin.length);
-
-  message.channel.send("**" + message.author.username + ",** I FLIPPED **" + coin[coinrandom] + "!**");
-
-  }
   
 }
 
