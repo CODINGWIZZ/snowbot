f07045b7c9f0f16cf3957ff5982568f5bc2ce19c
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-23 17:38:33 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
         let helpEmbed = new Discord.RichEmbed()
         .setColor(botconfig.blue)
         .setTitle("HELP ❆")
-        .setDescription("YOU CAN SEE ALL OF MY COMMANDS **[HERE]**(https://discordsnowbot.weebly.com/snow) AND HOW TO USE THEM**!**\nDOWN HERE, YOU'LL SEE ALL COMMANDS FOR SNOW __**RIGHT NOW**__**!**")
+        .setDescription("YOU CAN SEE ALL OF MY COMMANDS [**HERE**](https://discordsnowbot.weebly.com/snow) AND HOW TO USE THEM**!**\nDOWN HERE, YOU'LL SEE ALL COMMANDS FOR SNOW __**RIGHT NOW**__**!**")
         .addField("GENERAL", "`ping` `roll` `say` `snow` `invite` `changelog` `help`")
         .addField("MODERATION", "`addrole` `removerole` `ban` `kick` `clear` `warn` `mute` `deafen` `tempmute` `unmute` `tempdeafen` `undeafen`")
         .addField("FUN", "`rps` `avatar` `8ball` `vote` `slot` `randomcolor` `color` `fancy` `randomimage` `urban` `gif` `dog` `cat` `reminder`")
