026bc1869894ed33a7bc5809839d6144c2f7b557
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-24 23:31:59 +0200

    Update removerole.js

--- commands/removerole.js
+++ commands/removerole.js
@@ -20,12 +20,12 @@ module.exports.run = async (bot, message, args) => {
     if(!role) return message.channel.send("SPECIFY A ROLE**!**");
     let gRole = message.guild.roles.find(`name`, role);
     if(!gRole) message.channel.send("CAN'T FIND ROLE**!**");
-    // if(rMember.highestRole.position >= message.member.highestRole.position) return message.channel.send("CAN NOT GIVE A ROLE THAT'S HIGHER UP THAN YOURSELF**!**");
+    //if(bot.user.highestRole.position >= rMember.highestRole.position) return message.channel.send("CAN NOT GIVE A ROLE THAT'S HIGHER UP THAN YOURSELF**!**");
 
     if(rMember.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT REMOVE A ROLE FROM A MEMBER WITH THE SAME OR A HIGHER ROLE AS YOU**!**");        
     if(!rMember.roles.has(gRole.id)) return message.channel.send("THAT USER DOESN'T HAVE THAT ROLE**!**");
     await(rMember.removeRole(gRole.id)).then(() => {
-        message.channel.send(`<:SNOWCHECK:459111379899514887> **//** <@${rMember.id}> WAS REMOVED FROM THE **${gRole}** ROLE**!**`);
+        message.channel.send(`<@${rMember.id}> HAS BEEN REMOVED FROM THE **${gRole}** ROLE**!**`);
 
         let removeroleEmbed = new Discord.RichEmbed()
         .setColor(botconfig.blue)
