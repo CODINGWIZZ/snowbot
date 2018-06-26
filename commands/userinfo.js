000bb611e0041d1cff1afc1a69856cbf7b688a10
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-26 17:28:05 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -29,7 +29,8 @@ module.exports.run = async (bot, message, args) => {
      return r.name 
     }
     
-  }
+    return null;
+  })).join(" | ") : "NONE", false)
                                                                             
 
   message.channel.send(userinfoEmbed);
