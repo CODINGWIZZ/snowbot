2d59f0985d6d4bc2a0944836b363d95f9cda2b78
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-26 18:58:00 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,1 +1,34 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
 
+module.exports.run = async (bot, message, args) => {
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0].toLocaleLowerCase();
+  
+  // USER INFO
+  if(cmd === `${prefix}userinfo`) {
+
+  let user = message.mentions.users.first() || message.guild.members.get(args [0]) || message.author;
+  
+  let userinfoEmbed = new Discord.RichEmbed()
+  .setColor(botconfig.blue)
+  .setDescription("USER INFO **❆** **// " + user.username + "**")
+  .addField("** **", `${user.presence.game ? `Playing **${user.presence.game.name}**` : "NOT PLAYING ANYTHING**!**"}`)
+  .addField("FULL NAME", `**${user.username}**#${user.discriminator}`)
+  .addField("ID", user.id)
+  .addField("NICKNAME", `${user.nickname !== null ? `NICKNAME: ${user.nickname}` : "NO NICKNAME**!**"}`)
+  .addField("STATUS", user.presence.status.toUpperCase())
+  .addField("CREATED", user.createdAt.toDateString())
+  .setFooter("USER INFO | SNOW ❆", bot.user.displayAvatarURL);
+            
+  message.channel.send(userinfoEmbed);
+  
+    }
+
+}
+
+module.exports.help = {
+  name: "userinfo"
+}
