0ae350abba9193219261a5b9074b0dea8703a872
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-26 12:48:22 +0200

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -7,17 +7,12 @@ module.exports.run = async (bot, message, args) => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0].toLocaleLowerCase();
   
-   // INVITE SNOW
-   if(cmd === `${prefix}invite`) {
-
-   let inviteembed = new Discord.RichEmbed()
-   .setColor(botconfig.blue)
-   .setDescription("`INVITE SNOW:`\nhttps://bit.do/snowbot\n`TWITTER:`\nhttps://bit.do/snowtwitter\n`WIZZ TWITTER:`\nhttps://bit.do/codingwizz")
-   .setFooter("INVITE BOT | SNOW ❆", bot.user.displayAvatarURL);
-
-   message.channel.send(inviteembed);
-    
-  }
+  let inviteEmbed = new Discord.RichEmbed()
+  .setColor(botconfig.blue)
+  .setDescription("[**INVITE SNOW**](https://discordapp.com/oauth2/authorize?client_id=417210018576990208&scope=bot&permissions=8)\n\n[**TWITTER**](https://twitter.com/DISCORDSNOWBOT)\n\n[**WIZZ TWITTER**](https://twitter.com/CODINGWIZZ)")
+  .setFooter("INVITE BOT | SNOW ❆", bot.user.displayAvatarURL);
+  
+  message.channel.send(inviteEmbed);
 
 }
 
