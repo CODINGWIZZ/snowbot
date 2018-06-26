0a8fc41dcfb56283f22c97e70fcb30f5d62a2ccb
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-26 12:53:54 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -25,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
         
     let kickembed = new Discord.RichEmbed()
     .setColor(botconfig.blue)
-    .setTitle("KICK ❆")
+    .setDescription("KICK **❆**")
     .addField("USER", kUser)
     .addField("MODERATOR", message.author)
     .addField("CHANNEL", message.channel)
