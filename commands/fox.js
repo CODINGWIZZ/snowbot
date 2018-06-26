642d3570dbfd71cd0452a03a14e555bc5473bfb6
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-26 23:50:37 +0200

    Create fox.js

---
+++ commands/fox.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const botconfig = require("../botconfig.json");
+
+module.exports.run = async (bot, message, args) => {
+
+  let prefix = botconfig.prefix;
+  let messageArray = message.content.split(" ");
+  let messageArray[0].toLocaleLowerCase();
+  
+  if(cmd === `${prefix}fox`) {
+  
+  let foxlinkrandom = Math.floor((Math.random()) * 120);
+  let foxlink = `http://randomfox.ca/images/${foxlinkrandom}.jpg`;
+  
+  let foxEmbed = new Discord.RichEmbed()
+  .setColor(botconfig.blue)
+  .setDescription("FOX **❆**")
+  .setImage(foxlink);
+  
+  message.channel.send(foxEmbed);
+  
+  }
+
+}
+
+module.exports.help = [
+  name: "fox"
+}
