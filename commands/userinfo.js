1dbee3adb70e161eb47269b6cd03fafc37e40501
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-26 18:37:25 +0200

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -14,12 +14,11 @@ module.exports.run = async (bot, message, args) => {
   
   let userinfoEmbed = new Discord.RichEmbed()
   .setColor(botconfig.blue)
-  .setDescription("USER INFO **❆**")
+  .setDescription("USER INFO **❆** **//" + user.username + "**")
   .addField("** **", `${user.presence.game ? `${user.presence.game.name}` : "NOT PLAYING ANYTHING**!**"}`)
-  .addField("** **",  `${user.username}`)
   .addField("FULL NAME", `**${user.username}**#${user.discriminator}`)
   .addField("ID", user.id)
-  .addField("NICKNAME", user.nickname ? user.nickname : "NO NICKNAME")
+  .addField("NICKNAME", `${user.nickname !== null ? `NICKNAME: ${user.nickname}` : "NO NICKNAME**!**"})
   .addField("STATUS", user.presence.status.toUpperCase())
   .addField("CREATED", user.createdAt.toDateString())
   .setFooter("USER INFO | SNOW ❆", bot.user.displayAvatarURL);
