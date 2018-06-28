bf0e2578955ab011e501656a841c6fb7f0a83d67
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-06-28 22:26:05 +0200

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -27,7 +27,7 @@ module.exports.run = async (bot, message, args) => {
     message.channel.send(translateEmbed);
     
     }).catch(err => {
-      return message.channel.send("`" + transto.toUpperCase() +"` IS NOT VALID**!**");
+      return message.channel.send("`" + transto.toUpperCase() +"` IS NOT VALID COUNTRY CODE**!**");
     });
   
   }
