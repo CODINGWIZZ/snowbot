7594404fcd1a98ea59d46702259b6483f4db1fb2
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-07-25 21:12:24 +0200

    BING

---
+++ snowcommands/bing.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+const encode = require("strict-uri-encode");
+
+module.exports.run = async (bot, message, args) => {
+
+    let bing = encode(args.join(" "));
+    if(!bing) return message.channel.send("PLEASE ENTER A SEARCH QUERY**!**");
+
+    let binglink = `https://bing.com/search?q${bing}`;
+
+    message.channel.send("SEARCHING **...**").then((bingMessage) => {
+
+        bingMessage.edit("**FINISHED!**\n" + `<${binglink}>`);
+
+    });
+
+}
+
+module.exports.help = {
+    name: "bing"
+}
