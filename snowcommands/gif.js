d4ea65d0a0f16d0a9905878eef3a9515d98786e3
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-07-25 21:17:59 +0200

    GIF

---
+++ snowcommands/gif.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+const encode = require("strict-uri-encode");
+const got = require("got");
+const API_KEY = "SJBFr7rJmHOiLhZfF2KGGnOGTOqJI0kO";
+
+module.exports.run = async (bot, message, args) => {
+
+    let prefix = snow.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0].toLocaleLowerCase();
+
+    if(cmd === `${prefix}gif`) {
+
+        let gif = encode(args.join(" "));
+        if(!gif) return message.channel.send("PLEASE ENTER A GIF QUERY TO SEARCH FOR**!**");
+
+        const res = await got(`https://api.giphy.com/v1/gifs/random?api_key${API_KEY}?tag=${gif}`, { json: true });
+
+        message.channel.send("SEARCHING GIFS **...**").then((gifMessage) => {
+
+            if(!res || !res.body || !res.body.data) return message.channel.send("FAILED TO LOAD GIF**!**");
+
+            let gifEmbed = new Discord.RichEmbed()
+            .setColor(snow.blue)
+            .setDescription("GIF **❆**")
+            .setImage(res.body.data.image_url);
+
+            gifMessage.edit(gifEmbed);
+
+        });
+
+    }
+
+}
+
+module.exports.help = {
+    name: "gif"
+}
