a657641775db0c5a64a9a8d8de34999a1e8fd361
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-07-25 21:28:58 +0200

    SERVERINFO

---
+++ snowcommands/serverinfo.js
@@ -0,0 +1,87 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    let prefix = snow.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0].toLocaleLowerCase();
+
+    if(cmd === `${prefix}serverinfo`) {
+
+        let vertification = {
+
+            0: "NONE",
+            1: "LOW",
+            2: "MEDIUM",
+            3: "(╯°□°）╯︵ ┻━┻",
+            4: " ┻━┻彡 ヽ(ಠ益ಠ)ノ彡┻━┻"
+
+        };
+
+        let snowonline = "<:SNOWONLINE:461875150892171274>";
+        let snowidle = "<:SNOWIDLE:461875150896496660>";
+        let snowdnd = "<:SNOWDND:461875150716010497>";
+        let snowoffline = "<:SNOWOFFLINE:>461875151328378890";
+
+        let presences = message.guild.presences.map(st => st.status);
+
+        let onlinemembers = 0;
+        let idlemembers = 0;
+        let dndmembers = 0;
+
+        for(const i in presences) {
+
+            if(presences[i] === "online") {
+
+                onlinemembers += 1;
+
+            }
+
+        }
+
+        for(const i in presences) {
+
+            if(presences[i] === "idle") {
+
+                idlemembers += 1;
+
+            }
+
+        }
+
+        for(const i in presences) {
+
+            if(presences[i] === "dnd") {
+
+                dndmembers += 1;
+
+            }
+
+        }
+
+        let offlinemembers = message.guild.memberCount - onlinemembers - idlemembers - dndmembers;
+
+        let serverinfoEmbed = new Discord.RichEmbed()
+        .setColor(snow.blue)
+        .addField("** **", `**${message.guild.name}** // **(** ${message.guild.id} **)**`)
+        .setThumbnail(message.guild.iconURL)
+        .addField("OWNER", message.guild.owner + ` // **(** ${message.guild.owner.id} **)**`)
+        .addField("CREATED", message.guild.createdAt.toDateString())
+        .addField("VERTIFICATION LEVEL", vertification[message.guild.vertificationLevel])
+        .addField("REGION", message.guild.region.toUpperCase(), true)
+        .addField("CHANNELS", message.guild.channels.size, true)
+        .addField("TOTAL MEMBERS", message.guild.memberCount, true)
+        .addField("CURRENT MEMBER PRESENCES", snowonline + " **//** " + onlinemembers + "\n" + snowidle + " **//** " + idlemembers + "\n" + snowdnd + " **//** " + dndmembers + "\n" + snowoffline + " **//** " + offlinemembers)
+        .addField("TOTAL ROLES", message.guild.roles.size, true)
+        .setFooter("SERVER INFO | SNOW ❆", bot.user.displayAvatarURL);
+
+        message.channel.send(serverinfoEmbed);
+
+    }
+
+}
+
+module.exports.help = {
+    name: "serverinfo"
+}
