c5b3a21a72f2c3317a98d533e1c5871716399c49
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-07-26 12:17:35 +0200

    Update userinfo.js

--- snowcommands/userinfo.js
+++ snowcommands/userinfo.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
         let snowonline = "<:SNOWONLINE:461875150892171274>";
         let snowidle = "<:SNOWIDLE:461875150896496660>";
         let snowdnd = "<:SNOWDND:461875150716010497>";
-        let snowoffline = "<:SNOWOFFLINE:>461875151328378890";
+        let snowoffline = "<:SNOWOFFLINE:461875151328378890>";
 
         let status = {
 
@@ -27,8 +27,7 @@ module.exports.run = async (bot, message, args) => {
 
         let userinfoEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
-        .setDescription("USER INFO **❆** **// " + user.username + "**")
-        .addField("** **", `${user.presence.game ? `Playing **${user.presence.game.name}**` : "NOT PLAYING ANYTHING**!**"}`)
+        .setDescription("USER INFO **❆** **// " + user.username + "**\n" + `${user.presence.game ? `Playing **${user.presence.game.name}**` : "NOT PLAYING ANYTHING**!**"}`)
         .addField("FULL NAME", `**${user.username}**#${user.discriminator}`)
         .addField("ID", user.id)
         .addField("STATUS", `${status[user.presence.status]}`)
