cee30b809a299c7e65b824bd6f0f4f4c71235997
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-07-27 20:10:36 +0200

    CAT

---
+++ snowcommands/cat.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+
+    let prefix = snow.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0].toLocaleLowerCase();
+    
+    if(cmd === `${prefix}cat`) {
+    
+        const { body, header } = await superagent
+        .get("https://aws.random.cat//meow");
+
+        message.channel.send("GENERATING CAT IMAGE **...**").then((catMessage) => {
+
+            let catEmbed = new Discord.RichEmbed()
+            .setColor(snow.blue)
+            .setDescription(":cat: **//** CAT **❆**")
+            .setImage(body.file);
+
+            catMessage.edit(catEmbed);
+
+        });
+    
+    }
+
+}
+
+module.exports.help = {
+    name: "cat"
+}
