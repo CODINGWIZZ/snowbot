93f33bba201a1275f390c259842be3423b5e9e5a
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-07-27 19:57:16 +0200

    DOG

---
+++ snowcommands/dog.js
@@ -0,0 +1,34 @@
+const Discord = require("discord-js");
+const snow = require("../snow.json");
+
+const superagent = require("superagent");
+
+module.exports.run = async (bot, message, args) => {
+
+    let prefix = snow.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0].toLocaleLowerCase();
+    
+    if(cmd === `${prefix}dog`) {
+    
+    let { body } = await superagent
+    .get("https://random.dog/woof.json");
+
+    message.channel.send("GENERATING DOG IMAGE **...**").then((dogMessage) => {
+
+        let dogEmbed = new Discord.RichEmbed()
+        .setColor(snow.blue)
+        .setDescription(":dog: **//** DOG **❆**")
+        .setImage(body.url)
+
+        dogMessage.edit(dogEmbed);
+
+        }); 
+    
+    }
+    
+}
+
+module.exports.help = {
+    name: "dog"
+}
