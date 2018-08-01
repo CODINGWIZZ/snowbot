10c0a81d77a9127e07f13cda57a1fe6dcd21d509
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-08-01 16:20:54 +0200

    COLOR

--- snowcommands/color.js
+++ snowcommands/color.js
@@ -13,16 +13,18 @@ module.exports.run = async (bot, message, args) => {
 
         let color = args[0];
         if(!color) return message.channel.send("PLEASE ENTER A HEX COLOR YOU WANT TO CHECK**!**");
+        
+        if(color.startsWith("#")) color.slice(1);
     
-        if(!color.startsWith("#")) return message.channel.send("PLEASE ENTER A VALID HEX COLOR WITH A `#` IN THE BEGINNING**!**");
+//         if(!color.startsWith("#")) return message.channel.send("PLEASE ENTER A VALID HEX COLOR WITH A `#` IN THE BEGINNING**!**");
     
-        if(color.length > 7) return message.channel.send("PLEASE ENTER A VALID HEX COLOR**!**");
-        if(color.length < 7) return message.channel.send("PLEASE ENTER A VALID HEX COLOR**!**");
+        if(color.length > 6) return message.channel.send("PLEASE ENTER A VALID HEX COLOR**!**");
+        if(color.length < 6) return message.channel.send("PLEASE ENTER A VALID HEX COLOR**!**");
     
         let colorEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
-        .setAuthor(`${color.toUpperCase()}`, `https://dummyimage.com/250/${color.slice(1)}/&text=%20`)
-        .setDescription(`[[**INFORMATION**]](https://colorhexa.com/${color.slice(1)})` + "\n\n" + `**RGB**\n${convert.hex.rgb(color)}\n\n**HSL**\n${convert.hex.hsl(color)}\n\n**CSS**\n${convert.hex.keyword(color).toUpperCase()}`)
+        .setAuthor(`${color.toUpperCase()}`, `https://dummyimage.com/250/${color}/&text=%20`)
+        .setDescription(`[[**INFORMATION**]](https://colorhexa.com/${color})` + "\n\n" + `**RGB**\n${convert.hex.rgb(color)}\n\n**HSL**\n${convert.hex.hsl(color)}\n\n**CSS**\n${convert.hex.keyword(color).toUpperCase()}`)
         .setFooter("COLOR | SNOW ❆", bot.user.displayAvatarURL);
     
         message.channel.send(colorEmbed);
