8eb0441e17d2cbc953650e4c60b46d819c0e6a1b
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-08-06 13:07:43 +0200

    BING

--- snowcommands/bing.js
+++ snowcommands/bing.js
@@ -4,17 +4,25 @@ const snow = require("../snow.json");
 const encode = require("strict-uri-encode");
 
 module.exports.run = async (bot, message, args) => {
+    
+    let prefix = snow.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0].toLocaleLowerCase();
+    
+    if(cmd === `${prefix}bing`) {
+        
+         let bing = encode(args.join(" "));
+         if(!bing) return message.channel.send("PLEASE ENTER A SEARCH QUERY**!**");
 
-    let bing = encode(args.join(" "));
-    if(!bing) return message.channel.send("PLEASE ENTER A SEARCH QUERY**!**");
+         let binglink = `https://bing.com/search?q${bing}`;
 
-    let binglink = `https://bing.com/search?q${bing}`;
+         message.channel.send("SEARCHING **...**").then((bingMessage) => {
 
-    message.channel.send("SEARCHING **...**").then((bingMessage) => {
-
-        bingMessage.edit("**FINISHED!**\n" + `<${binglink}>`);
+         bingMessage.edit("**FINISHED!**\n" + `<${binglink}>`);
 
     });
+        
+    }
 
 }
 
