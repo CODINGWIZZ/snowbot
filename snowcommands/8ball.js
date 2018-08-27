da46a46842a0e373626ff75c736f43b33690d613
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-08-27 14:43:07 +0200

    8BALL

--- snowcommands/8ball.js
+++ snowcommands/8ball.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
 
         if(!args[0]) return message.channel.send("PLEASE ASK A FULL QUESTION WITH AT LEAST **2** WORDS**!**");
 
-        let answers = ["NO", "NOT TODAY", "IT IS DECIDEDLY SO", "WITHOUT A DOUBT", "YES **//** DEFINITELY", "YOU MAY RELY ON IT", "AS I SEE IT YES", "MOST LIKELY", "OUTLOOK GOOD", "SIGnS POINT TO YES", "TRY AGAIN", "ASK AGAIN LATER", "BETTER NOT TELL YOU RIGHT NOW", "CAN NOT PREDICT RIGHT NOW", "CONCENTRATE AND ASK AGAIN", "DON'T COUNT ON IT", "MY REPLY IS NO", "MY SOURCES SAY NO", "OUTLOOK NOT SO GOOD", "VERY DOUBTFUL"];
+        let answers = ["NO", "NOT TODAY", "IT IS DECIDEDLY SO", "WITHOUT A DOUBT", "YES **//** DEFINITELY", "YOU MAY RELY ON IT", "AS I SEE IT YES", "MOST LIKELY", "OUTLOOK GOOD", "SIGNS POINT TO YES", "TRY AGAIN", "ASK AGAIN LATER", "BETTER NOT TELL YOU RIGHT NOW", "CAN NOT PREDICT RIGHT NOW", "CONCENTRATE AND ASK AGAIN", "DON'T COUNT ON IT", "MY REPLY IS NO", "MY SOURCES SAY NO", "OUTLOOK NOT SO GOOD", "VERY DOUBTFUL"];
         let answerresult = Math.floor((Math.random()) * answers.length);
 
         message.channel.send(`:8ball: **// ${message.author.username},** ${answers[answerresult]}`);
