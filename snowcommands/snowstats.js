a67e4be64daeec829660c30326cca5fc21672190
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-08-27 16:29:54 +0200

    SNOWSTATS

---
+++ snowcommands/snowstats.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    let prefix = snow.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0].toLocaleLowerCase();
+
+    if(cmd === `${prefix}snow`) {
+
+        let snowEmbed = new Discord.RichEmbed()
+        .setColor(snow.blue)
+        .setDescription("SNOW INFORMATION **❆**")
+        .addField("BOT NAME", "**" + snow.name + "**#" + bot.user.discrimiator)
+        .addField("DEVELOPER", "**" + snow.dev + "**#7897")
+        .addField("VERSION", snow.version)
+        .addField("WEBSITE", "discordsnowbot.weebly.com")
+        .addField("STATS", `**${bot.guild.size} SERVERS\n\n${bot.channels.size} CHANNELS\n\n${bot.users.size} USERS**`)
+        .setFooter("BOT STATS | SNOW ❆", bot.user.displayAvatarURL);
+        
+        message.channel.send(snowEmbed);
+
+    } 
+
+}
+
+module.exports.help = {
+    name: "snow"
+}
