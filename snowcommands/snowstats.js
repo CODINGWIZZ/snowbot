fe96a809cd00b69f9f950459bd5699beb097c5cd
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-08-27 16:55:04 +0200

    SNOWSTATS

--- snowcommands/snowstats.js
+++ snowcommands/snowstats.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
         .setColor(snow.blue)
         .setDescription("SNOW INFORMATION **❆**")
         .addField("BOT NAME", "**" + snow.name + "**#" + bot.user.discriminator + " **(** " + bot.user.id + " **)**")
-        .addField("DEVELOPER", "**" + snow.dev + "**#7897 **(** 297832577782382592 **)**")
+        .addField("DEVELOPER", "**" + snow.dev + "**#7897")
         .addField("VERSION", "**" + snow.version + "**")
         .addField("WEBSITE", "https://discordsnowbot.weebly.com/") 
         .addField("STATS", `**${bot.guilds.size}** SERVERS\n**${bot.channels.size}** CHANNELS\n**${bot.users.size}** USERS`)
