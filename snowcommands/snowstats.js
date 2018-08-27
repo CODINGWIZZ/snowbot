6b993c9f969f1d3ffb20c7876c13e5ed5425203c
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-08-27 16:48:31 +0200

    SNOWSTATS

--- snowcommands/snowstats.js
+++ snowcommands/snowstats.js
@@ -12,10 +12,10 @@ module.exports.run = async (bot, message, args) => {
         let snowEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
         .setDescription("SNOW INFORMATION **❆**")
-        .addField("BOT NAME", "**" + snow.name + "**#" + bot.user.discrimiator)
+        .addField("BOT NAME", "**" + snow.name + "**#" + bot.user.discriminator)
         .addField("DEVELOPER", "**" + snow.dev + "**#7897")
-        .addField("VERSION", snow.version)
-        .addField("WEBSITE", "discordsnowbot.weebly.com")
+        .addField("VERSION", "**" + snow.version + "**")
+        .addField("WEBSITE", "https://discordsnowbot.weebly.com/")
         .addField("STATS", `**${bot.guilds.size} SERVERS\n\n${bot.channels.size} CHANNELS\n\n${bot.users.size} USERS**`)
         .setFooter("BOT STATS | SNOW ❆", bot.user.displayAvatarURL);
         
