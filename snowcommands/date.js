f348c3a9ff2b7afd745745e2276160b219fd655d
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-08-28 11:43:47 +0200

    DATE

--- snowcommands/date.js
+++ snowcommands/date.js
@@ -11,7 +11,7 @@ module.exports.run = async (bot, message, args) => {
         
         let date = new Date();
     
-        message.channel.send(date);
+        message.channel.send(date.toUTCString());
     
     }
 
