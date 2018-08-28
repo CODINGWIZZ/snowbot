ab883502a54e877312b1d5500e5841218ba3840a
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-08-28 11:02:19 +0200

    DATE

---
+++ snowcommands/date.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+const snow = reqire("../snow.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    let prefix = snow.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0].toLocaleLowerCase();
+    
+    if(cmd === `${prefix}date`) {
+    
+        message.channel.send(new Date());
+    
+    }
+
+}
+
+module.exports.help = {
+    name: "date"
+}
