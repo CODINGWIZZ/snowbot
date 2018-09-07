62164caf40f04d942c7fc366d8409aecd844d0fb
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:39:31 +0200

    KICK

--- snowcommands/kick.js
+++ snowcommands/kick.js
@@ -1,44 +1,39 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
+    if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    
+    if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO KICK AND THEN A REASON**!**");
 
-    if(cmd === `${prefix}kick`) {
+    let kUser = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
+    if(!kUser) return message.channel.send("CAN'T FIND USER**!**");
+    if(kUser.id === message.author.id) return message.channel.send("YOU CAN NOT KICK YOURSELF**!**");
+    if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("THIS USER CAN'T BE KICKED**!**");
+    if(kUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT KICK A MEMBER WHO HAS A HIGHER OR THE SAME ROLE AS YOU**!**");
 
-        if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    let kickreason = args.slice(1).join(" ");
+    if(!kickreason) return message.channel.send("PLEASE INCLUDE A REASON FOR THE KICK**!**");
 
-        if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO KICK**!**");
-    
-        let kUser = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
-        if(!kUser) return message.channel.send("CAN'T FIND USER**!**");
-        if(kUser.id === message.author.id) return message.channel.send("YOU CAN NOT KICK YOURSELF**!**");
-        if(kUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("THIS USER CAN'T BE KICKED**!**");
-    
-        let kickreason = args.slice(1).join(" ");
-        if(!kickreason) return message.channel.send("PLEASE INCLUDE A REASON FOR THE KICK**!**");
-        
-        message.guild.member(kUser).kick(kickreason);
-        message.channel.send(`${kUser} HAS BEEN **KICKED** BY ${message.author} BECAUSE**: ${kickreason}!**`);
-    
-        let kickEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setDescription("KICK **❆**")
-        .addField("USER", kUser)
-        .addField("MODERATOR", message.author)
-        .addField("CHANNEL", message.channel)
-        .addField("REASON", kickreason)
-        .setFooter("SNOW ❆", bot.user.displayAvatarURL);
-    
-        let snowlog = message.guild.channels.find(`name`, "snow");
-        if(!snowlog) return;
-    
-        snowlog.send(kickEmbed);
+    message.guild.memebr(kUser).kick(kickreason);
+    message.channel.send(`${kUser} HAS BEEN **KICKED** BY ${message.author} BECAUSE**: ${kickreason}**`);
 
-    }
+    let kickEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setDescription("KICK **" + snow.snowflake + "**")
+    .addField("USER", kUser)
+    .addField("MODERATOR", message.author)
+    .addField("CHANNEL", message.channel)
+    .addField("REASON", kickreason)
+    .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+
+    let snowlog = message.guild.channels.find(`name`, "snow");
+    if(!snowlog) return;
+
+    snowlog.send(kickEmbed);
 
 }
 
