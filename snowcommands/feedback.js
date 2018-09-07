5669d4d825e6fffff50043ee498d6ce4fb1ec234
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:38:07 +0200

    FEEDBACK

--- snowcommands/feedback.js
+++ snowcommands/feedback.js
@@ -1,28 +1,22 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
-    
-    if(cmd === `${prefix}feedback`) {
-    
-        let feedback = args.join(" ");
-        if(!feedback) return message.channel.send("PLEASE ENTER A MESSAGE THAT YOU WANT TO SEND LIKE FEEDBACK**!**");
-        
-        let feedbackEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setDescription("FEEDBACK **❆**")
-        .addField("WRITTEN BY", message.author + " **// " + message.author.username + "**#" + message.author.discriminator)
-        .addField("FEEDBACK", feedback)
-        .setFooter("FEEDBACK | SNOW ❆", bot.user.displayAvatarURL);
-        
-        bot.channels.get("483909335513301002").send(feedbackEmbed);
-        message.channel.send("THANKS **" + message.author.username + "** FOR THE FEEDBACK**!** WE'LL TRY TO MAKE YOUR WISH COME TRUE**!**");
-    
-    }
+    let feedback = args.join(" ");
+    if(!feedback) return message.channel.send("PLEASE ENTER A MESSAGE THAT YOU WANT TO SEND LIKE A FEEDBACK FORM**!**");
+
+    let feedbackEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setDescription("FEEDBACK **" + snow.snowflake + "**")
+    .addField("WRITTEN BY", message.author + " **// " + message.author.username + "**#" + message.author.discriminator)
+    .addField("FEEDBACK", feedback)
+    .setFooter("FEEDBACK | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+
+    bot.channels.get("483909335513301002").send(feedbackEmbed);
+    message.channel.send("THANKS **" + message.author.username + "** FOR THE FEEDBACK**!** WE'LL TRY TO MAKE YOUR WISH COME TRUE**!**");
 
 }
 
