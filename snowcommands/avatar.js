fa756d17e34722ca2d266fb34ae039838854d59a
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:07:42 +0200

    AVATAR

--- snowcommands/avatar.js
+++ snowcommands/avatar.js
@@ -1,25 +1,19 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
-    
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
 
-    if(cmd === `${prefix}avatar`) {
+    let avatar = message.mentions.users.first() || message.author;
+    if(!avatar.avatarURL) return message.channel.send("CAN'T FIND THE AVATAR FOR THE DESIRED USER**!**");
 
-        let avatar = message.mentions.users.first() || message.author;
-        if(!avatar.avatarURL) return message.channel.send("CAN'T FIND THE AVATAR FOR THE DESIRED USER**!**");
+    let avatarEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setDescription("AVATAR **" + snow.snowflake + " // " + avatar.username + `**\n[[**DOWNLOAD**]](${avatar.avatarURL})`)
+    .setImage(avatar.avatarURL);
 
-        let avatarEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setDescription("AVATAR **❆ // " + avatar.username + `**\n[[**DOWNLOAD**]](${avatar.avatarURL})`)
-        .setImage(avatar.avatarURL);
-        
-        message.channel.send(avatarEmbed);
-
-    }
+    message.channel.send(avatarEmbed);
 
 }
 
