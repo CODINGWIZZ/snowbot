b133a53a20574cc746d1e64b1ca8149b6514be25
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:40:12 +0200

    MUTE

--- snowcommands/mute.js
+++ snowcommands/mute.js
@@ -3,95 +3,89 @@ const snow = require("../snow.json");
 
 const ms = require("ms");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
+    if(!message.member.hasPermission("MUTE_MEMERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
 
-    if(cmd === `${prefix}mute`) {
+    if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO MUTE AND THEN HOW LONG**!**");
 
-        if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    let mUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(mUser.id === message.author.id) return message.channel.send("YOU CAN NOT MUTE YOURSELF**!**");
+    if(mUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT MUTE A MEMBER WHO HAS A HIGHER OR THE SAME ROLE AS YOU**!**");
 
-        if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO MUTE**!**");
+    let muterole = message.guild.roles.find(`name`, "MUTED // " + snow.snowflake);
+    if(!muterole) {
 
-        let mUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(mUser.id === message.author.id) return message.channel.send("YOU CAN NOT MUTE YOURSELF**!**");
-        if(mUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT MUTE A MEMBER WHO HAS A HIGHER OR THE SAME ROLE AS YOU**!**");
+        try {
 
-        let muterole = message.guild.roles.find(`name`, "MUTED // ❆");
-        if(!muterole) {
+            muterole = await message.guild.createRole({
+                name: "MUTED // " + snow.snowflake,
+                color: "#65798d",
+                permissions: []
+            })
 
-            try {
+            message.guild.channels.forEach(async (channel, id) => {
 
-                muterole = await message.guild.createRole({
-                    name: "MUTED // ❆",
-                    color: "#65798d",
-                    permissions: []
-                })
-
-                message.guild.channels.forEach(async (channel, id) => {
-
-                    await channel.overwritePermissions(muterole, {
-                        SEND_MESSAGES: false,
-                        ADD_REACTIONS: false,
-                        SPEAK: false
-                    });
-
+                await channel.overwritePermissions(muterole, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false,
+                    SPEAK: false
                 });
 
-            } catch (e) {
+            });
 
-                console.log(e.stack);
+        } catch (e) {
 
-            } 
+            console.log(e.stack);
 
         }
 
-        if(mUser.roles.has(muterole.id)) return message.channel.send("THIS USER IS ALREADY MUTED**!**");
-
-        let mutetime = args[1];
-        if(!mutetime) return message.channel.send("SPECIFY A TIME**!**");
-
-        await(mUser.addRole(muterole.id));
-        message.channel.send(`${mUser} HAS BEEN **MUTED** FOR **${ms(ms(mutetime))}!**`);
-
-        let muteEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setTimestamp()
-        .setDescription("MUTE **❆**")
-        .addField("USER", mUser)
-        .addField("MODERATOR", message.author)
-        .addField("CHANNEL", message.channel)
-        .addField("DURATION", ms(ms(mutetime)))
-        .setFooter("SNOW ❆", bot.user.displayAvatarURL);
-
-        let snowlog = message.guild.channels.find(`name`, "snow");
-        if(!snowlog) return;
-
-        snowlog.send(muteEmbed);
-
-        let autounmuteEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setTimestamp()
-        .setDescription("UNMUTE **❆**")
-        .addField("USER", mUser)
-        .addField("MODERATOR", `<@${bot.user.id}>`)
-        .addField("DURATION", ms(ms(mutetime)))
-        .setFooter("SNOW ❆", bot.user.displayAvatarURL);
-
-        let snowlog1 = message.guild.channels.find(`name`, "snow");
-        if(!snowlog1) return;
-
-        setTimeout(function () {
-
-            if(!mUser.roles.has(muterole.id)) return;
-            mUser.removeRole(muterole.id);
-            snowlog1.send(autounmuteEmbed);
-
-        }, ms(mutetime));
-
     }
+
+    if(mUser.roles.has(muterole.id)) return message.channel.send("THIS USER Is ALREADY MUTED**!**");
+
+    let mutetime = args[1];
+    if(!mutetime) return message.channel.send("PLEASE SPECIFY HOW LONG THIS USER SHOULD BE MUTED**!**");
+
+    await(mUser.addRole(muterole.id));
+    message.channel.send(`${mUser} HAS BEEN **MUTED** FOR **${ms(ms(mutetime))}!**`);
+
+    let muteEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setTimestam()
+    .setDescription("MUTE **" + snow.snowflake + "**")
+    .addField("USER", mUser)
+    .addField("MODERATOR", message.author)
+    .addField("CHANNEL", message.channel)
+    .addField("DURATION", ms(ms(mutetime)))
+    .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+
+    let snowlog = message.guild.channels.find(`name`, "snow");
+    if(!snowlog) return;
+
+    snowlog.send(muteEmbed);
+
+    let autounmuteEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setTimestamp()
+    .setDescription("UNMUTE **" + snow.snowflake + "**")
+    .addField("USER", mUser)
+    .addField("MODERATOR", `<@${bot.user.id}>`)
+    .addField("DURATION", ms(ms(mutetime)))
+    .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+
+    let snowlog1 = message.guild.channels.find(`name`, "snow");
+    if(!snowlog1) return;
+
+    setTimeout(function () {
+
+        if(!mUser.roles.has(muterole.id)) return;
+        mUser.removeRole(muterole.id);
+        snowlog1.send(autounmuteEmbed);
+ 
+    });
 
 }
 
