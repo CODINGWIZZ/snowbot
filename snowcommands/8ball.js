11f4e57a20ff78541bee710b6804bba0c0e4095e
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:06:07 +0200

    8BALL

--- snowcommands/8ball.js
+++ snowcommands/8ball.js
@@ -1,25 +1,19 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
+    let question = args.join(" ");
+    if(!question) return message.channel.send("PLEASE ENTER A QUESTION TO THE MAGIC **SNOW!**");
 
-    if(cmd === `${prefix}8ball`) {
+    if(!args[2]) return message.channel.send("PLEASE ASK A FULL QUESTION WITH AT LEAST **2** WORDS**!**");
 
-        let question = args.join(" ");
-        if(!question) return message.channel.send("PLEASE ENTER A QUESTION TO THE MAGIC SNOW**!**");
+    let answers = ["NO", "NOT TODAY", "IT IS DECIDEDLY SO", "WITHOUT A DOUBT", "YES **//** DEFINITELY", "YOU MAY RELY ON IT", "AS I SEE IT YES", "MOST LIKELY", "OUTLOOK GOOD", "SIGNS POINT TO YES", "TRY AGAIN", "TRY AGAIN LATER", "BETTER NOT TELL YOU RIGHT NOW", "CAN NOT PREDICT RIGHT NOW", "CONCENTRATE AND ASK AGAIN", "DON'T COUNT ON IT", "MY REPLY IS NO", "MY SOURCES SAY NO", "OUTLOOK NOT SO GOOD", "VERY DOUBTFUL"];
+    let answersresult = Math.floor((Math.random()) * answers.length);
 
-        if(!args[0]) return message.channel.send("PLEASE ASK A FULL QUESTION WITH AT LEAST **2** WORDS**!**");
-
-        let answers = ["NO", "NOT TODAY", "IT IS DECIDEDLY SO", "WITHOUT A DOUBT", "YES **//** DEFINITELY", "YOU MAY RELY ON IT", "AS I SEE IT YES", "MOST LIKELY", "OUTLOOK GOOD", "SIGNS POINT TO YES", "TRY AGAIN", "ASK AGAIN LATER", "BETTER NOT TELL YOU RIGHT NOW", "CAN NOT PREDICT RIGHT NOW", "CONCENTRATE AND ASK AGAIN", "DON'T COUNT ON IT", "MY REPLY IS NO", "MY SOURCES SAY NO", "OUTLOOK NOT SO GOOD", "VERY DOUBTFUL"];
-        let answerresult = Math.floor((Math.random()) * answers.length);
-
-        message.channel.send(`:8ball: **// ${message.author.username},** ${answers[answerresult]}`);
-
-    }
+    message.channel.send(`:8ball: **// ${message.author.username},** ${answers[answersresult]}`);
 
 }
 
