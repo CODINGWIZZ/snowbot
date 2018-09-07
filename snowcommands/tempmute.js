dc4bbf02254429947c8c9788f155fb7c7b569ea2
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:43:54 +0200

    TEMPMUTE

--- snowcommands/tempmute.js
+++ snowcommands/tempmute.js
@@ -1,75 +1,70 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
+    if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    
+    if(!args[0]) return message.channel.send("PLEASE MENTION A USER YOU WANT TO TEMPMUTE**!**");
 
-    if(cmd === `${prefix}tempmute`) {
+    let tmUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    if(!tmUser) return message.channel.send("CAN'T FIND USER**!**");
+    if(tmUser.id === message.author.id) return message.channel.send("YOU CAN NOT TEMPMUTE YOURSELF**!**");
+    if(tmUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT TEMPMUTE A MEMBER WHO HAS A HIGHER OR THE SAME ROLE AS YOU**!**");
 
-        if(!message.member.hasPermission("MUTE_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    let tempmuterole = message.guild.roles.find(`name`, "MUTED // " + snow.snowflake);
+    if(!tempmuterole) {
 
-        if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO TEMOMUTE**!**");
+        try {
 
-        let tmUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!tmUser) return message.channel.send("CAN'T FIND USER**!*");
-        if(tmUser === message.author.id) return message.channel.send("YOU CAN NOT TEMPMUTE YOURSELF**!**");
-        if(tmUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT TEMPMUTE A MEMBER WHO HAS A HIGHER OR THE SAME ROLE AS YOU**!**");
+            tempmuterole = await message.guild.createRole({
+                name: "MUTED // " + snow.snowflake,
+                color: "#65798d",
+                permissions: []
+            })
 
-        let tempmuterole = message.guild.roles.find(`name`, "MUTED // ❆");
-        if(!tempmuterole) {
+            message.guild.channels.forEach(async (channel, id) => {
 
-            try {
 
-                tempmuterole = await message.guild.createRole({
-                    name: "MUTED // ❆",
-                    color: "#65798d",
-                    permissions: []
-                })
-
-                message.guild.channels.forEach(async (channel, id) => {
-
-                    await channel.overwritePermissions(tempmuterole, {
-                        SEND_MESSAGES: false,
-                        ADD_REACTIONS: false,
-                        SPEAK: false
-                    });
-
+                await channel.overwritePermissions(tempmuterole, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false,
+                    SPEAK: false
                 });
 
-            } catch (e) {
+            });
 
-                console.log(e.stack);
+        } catch (e) {
 
-            }
+            console.log(e.stack);
 
         }
 
-        if(tmUser.roles.has(tempmuterole.id)) return message.channel.send("THIS USER IS ALREADY MUTED**!**");
-
-        awaitU(tmUser.addRole(tempmuterole.id));
-        message.channel.send(`${tmUser} HAS BEEN **TEMPMUTED!**`);
-
-        let tempmuteEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setTimestamp()
-        .setDescription("TEMPMUTE **❆**")
-        .addField("USER", tmUser)
-        .addField("MODERATOR", message.author)
-        .addField("CHANNEL", massage.channel)
-        .setFooter("SNOW ❆", bot.user.displayAvatarURL);
-
-        let snowlog = message.guild.channels.find(`name`, "snow");
-        if(!snowlog) return;
-
-        snowlog.send(tempmuteEmbed);
-
     }
 
+    if(tmUser.roles.has(tempmuterole.id)) return message.channel.send("THIS USER IS ALREADY MUTED**!**");
+
+    await(tmUser.addRole(tempmuterole.id));
+    message.channel.send(`${tmUser} HAS BEEN **TEMPMUTED!**`);
+
+    let tempmuteEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setTimestamp()
+    .setDescription("TEMPMUTE **" + snow.snowflake + "**")
+    .addField("USER", tmUser)
+    .addField("MODERATOR", message.author)
+    .addField("CHANNEL", message.channel)
+    .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+
+    let snowlog = message.guild.channels.find(`name`, "snow");
+    if(!snowlog) return;
+
+    snowlog.send(tempmuteEmbed);
+  
 }
 
 module.exports.help = {
-    name: "tempmute"
+    name: "tempdeafen"
 }
