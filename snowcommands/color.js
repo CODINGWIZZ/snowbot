7d50147532a277f504cde04cd8462f1226bf9264
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:08:52 +0200

    COLOR

--- snowcommands/color.js
+++ snowcommands/color.js
@@ -3,32 +3,26 @@ const snow = require("../snow.json");
 
 const convert = require("color-convert");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
-    
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
 
-    if(cmd === `${prefix}color`) {
+    let color = args[0].replace("#", "");
+    if(!color) return message.channel.send("PLEASE ENTER A HEX COLOR THAT YOU WANT TO CHECK**!**");
 
-        let color = args[0].replace("#", "");
-        if(!color) return message.channel.send("PLEASE ENTER A HEX COLOR YOU WANT TO CHECK**!**");
-   
-        if(color.length > 6) return message.channel.send("PLEASE ENTER A VALID HEX COLOR**!**");
-        if(color.length < 6) return message.channel.send("PLEASE ENTER A VALID HEX COLOR**!**");
-    
-        let colorEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setAuthor(`${color.toUpperCase()}`, `https://dummyimage.com/250/${color}/&text=%20`)
-        .setDescription(`[[**INFORMATION**]](https://colorhexa.com/${color})` + "\n\n" + `**RGB**\n${convert.hex.rgb(color)}\n\n**HSL**\n${convert.hex.hsl(color)}\n\n**CSS**\n${convert.hex.keyword(color).toUpperCase()}`)
-        .setFooter("COLOR | SNOW ❆", bot.user.displayAvatarURL);
-    
-        message.channel.send(colorEmbed);
+    if(color.length > 6) return message.channel.send("PLEASE ENTER A VALID HEX CODE**!**");
+    if(color.length < 6) return message.channel.send("PLEASE ENTAR A VALID HEX COLOR**!**");
 
-    }
-    
-} 
+    let colorEmbed = new Discord.RichEmbed()
+    .setColor(color)
+    .setAuthor(`${color.toUpperCase()}`, `https://dummyimage.com/250/${color}/&text=%20`)
+    .setDescription(`[[**INFORMATION**]](https://colorhexa.com/${color})` + "\n\n" + `**RGB**\n${convert.hex.rgb(color)}\n\n**HSL**\n${convert.hex.hsl(color)}\n\n**CSS**\n${convert.hex.keyword(color).toUpperCase()}`)
+    .setFooter("COLOR | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+
+    message.channel.send(colorEmbed);
+
+}
 
 module.exports.help = {
-    name: "color"
+    name: `color`
 }
