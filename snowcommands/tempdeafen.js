337e4d2129b8d8e60f9985592ea818ee4cf02716
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:43:36 +0200

    TEMPDEAFEN

--- snowcommands/tempdeafen.js
+++ snowcommands/tempdeafen.js
@@ -1,70 +1,61 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
+    if(!message.member.hasPermission("DEAFEN_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
 
-    if(cmd === `${prefix}tempdeafen`) {
+    if(!args[0]) return message.channel.send("PLEASE MENTION A USER YOU WANT TO TEMPDEAFEN**!**");
 
-        if(!message.member.hasPermission("DEAFAEN_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    let tdUser = message.guild.member(message.mentions.users.fist() || message.guild.members.get(args[0]));
+    if(!tdUser) return message.channel.send("CAN'T FIND USER**!**");
+    if(tdUser === message.guild.id) return message.channel.send("YOU CAN NOT TEMPDEAFEN YOURSELF**!**");
+    if(tdUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT TEMPDEAFEN A MEMBER WHO HAS A HIGHER OR THE SAME ROLE AS YOU**!**");
 
-        if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO  TEMPDEAFEN**!**");
+    let tempdeafenrole = message.guild.roles.find(`name`, "DEAFENED // " + snow.snowflake);
+    if(!tempdeafenrole) {
 
-        let tdUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!tdUser) return message.channel.send("CAN'T FIND USER**!**");
-        if(tdUser.id === message.author.id) return message.channel.send("YOU CAN NOT TEMPDEAFEN YOURSELF**!**");
-        if(tdUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT TEMPDEAFEN A MEMBER WHO HAS A HIGHER OR THE SAME ROLE AS YOU**!**");
+        try {
 
-        let tempdeafenrole = message.guild.roles.find(`name`, "DEAFENED // ❆");
-        if(!tempdeafenrole) {
+            tempdeafenrole = await message.guild.createRole({
+                name: "DEAFENED // " + snow.snowflake,
+                color: "#65798d",
+                permissions: []
+            })
 
-            try {
+            message.guild.channels.forEach(async (channel, id) => {
 
-                tempdeafenrole = await message.guild.createRole({
-                    name: "DEAFENED // ❆",
-                    color: "#65798d",
-                    permissions: []
-                })
-
-                message.guild.channels.forEach(async (channel, id) => {
-
-                    await channel.overwritePermissions(tempdeafenrole, {
-                        SPEAK: false
-                    });
-
+                await channel.overwritePermissions(tempdeafenrole, {
+                    SPEAK: false
                 });
 
-            } catch(e) {
+            });
 
-                console.log(e.stack);
+        } catch(e) {
 
-            }
+            console.log(e.stack);
 
         }
 
-        if(tdUser.roles.has(tempdeafenrole.id)) return message.channel.send("THIS USER IS ALREADY DEAFENED**!**");
-
-        await(tdUser.addRole(tempdeafenrole.id));
-        message.channel.send(`${tdUser} HAS BEEN TEMPDEAFENED**!**`);
-
-        let tempdeafenEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setTimestamp()
-        .setDescription("TEMPDEAFEN **❆**")
-        .addField("USER", tdUser)
-        .addField("MODERATOR", message.author)
-        .addField("CHANNEL", message.channel)
-        .setFooter("SNOW ❆", bot.user.displayAvatarURL);
-
-        let snowlog = message.guild.channels.find(`name`, "snow");
-        if(!snowlog) return;
-
-        snowlog.send(tempdeafenEmbed);
-
     }
+
+    if(tdUser.roles.has(tempdeafenrole.id)) return message.channel.send("THIS USER IS ALREADY DEAFENED**!**");
+
+    await(tdUser.addRole(tempdeafenrole.id));
+    message.channel.send(`${tdUser} HAS BEEN TEMPDEAFEN**!**`);
+
+    let tempdeafenEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setTimestamp()
+    .setDescription("TEMPDEAFEN **" + snow.snowflake + "**")
+    .addField("USER", tdUser)
+    .addField("MODERATOR", message.author)
+    .addField("CHANNEL", message.channel)
+    .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+
+    snowlog.send(tempdeafenEmbed);
 
 }
 
