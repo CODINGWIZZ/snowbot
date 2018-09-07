abc14400481aa6e96f4eb03af29075735374fe3e
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:07:07 +0200

    ANNOUNCE

--- snowcommands/announce.js
+++ snowcommands/announce.js
@@ -1,31 +1,25 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
 
-    if(cmd === `${prefix}announce`) {
+    let annoucechannel = message.mentions.channels.first();
+    if(!annoucechannel) return message.channel.send("PLEASE MENTION A CHANNEL AND THEN THE MESSAGE YOU WANT TO ANNOUNCE**!**");
 
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
-    
-        let announcechannel = message.mentions.channels.first();
-        if(!announcechannel) return message.channel.send("PLEASE MENTION A CHANNEL AND THE THE MESSAGE YOU WANT TO ANNOUNCE**!**");
+    let announce = args.slice(1).join(" ");
+    if(!announce) return message.channel.send("PLEASE ENTER A MESSAGE YOU WANT TO ANNOUNCE**!**");
 
-        let announce = args.slice(1).join(" ");
-        if(!announce) return message.channel.send("PLEASE ENTER A MESSAGE THAT YOU WANT TO ANNOUNCE**!**");
+    let announceEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setTimestamp()
+    .setDescription(annouce + "\n\n **(** ANNOUNCEMENT BY **" + message.author.username + " )**")
+    .setFooter("ANNOUNCE | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
 
-        let announceEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setTimestamp()
-        .setDescription(announce + "\n\n **(** ANNOUNCEMENT BY **" + message.author.username + " )**")
-        .setFooter("ANNOUNCE | SNOW ❆", bot.user.displayAvatarURL);
-
-        announcechannel.send(announceEmbed);
-
-    }
+    annoucechannel.send(announceEmbed);
 
 }
 
