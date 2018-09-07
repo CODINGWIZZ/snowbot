36073fd5d6334a6b7e7fda8115833fc2755d5dc8
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:09:15 +0200

    DATE

--- snowcommands/date.js
+++ snowcommands/date.js
@@ -1,25 +1,17 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
-    
-    if(cmd === `${prefix}date`) {
-        
-        let date = new Date();
-        
-        let dateEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setDescription("**" + date.toUTCString().toUpperCase() + "**");
-        
-        message.channel.send(dateEmbed);
-    
-//         message.channel.send(date.toUTCString().toUpperCase());
-    
-    }
+    let date = new Date();
+
+    let dateEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setDescription("**" + date.toUTCString().toUpperCase() + "**");
+
+    message.channel.send(dateEmbed);
 
 }
 
