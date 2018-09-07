3ec99c9e07c6df8aeb089eac9db6e0cc29476f14
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:45:54 +0200

    WARN

--- snowcommands/warn.js
+++ snowcommands/warn.js
@@ -1,54 +1,48 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
 
-    if(cmd === `${prefix}warn`) {
+    if(!args[0]) return message.channel.send("PLEASE MENTION A USER YOU WANT TO WARN AND THEN A REASON**!**");
 
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    let wUser = message.guild.member(message.mentions.users.first() ||message.guild.member.get(args[0]));
+    if(!wUser) return message.channel.send("CAN'T FIND USER**!**");
 
-        if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO WARN**!**");
+    if(wUser.id === message.author.id) return message.channel.send("YOU CAN NOT WARN YOURSELF**!**");
+    if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("THAT USER CAN'T BE WARNED**!**");
 
-        let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-        if(!wUser) return message.channel.send("CAN'T FIND USER**!**");
+    let warnreason = args.slice(1).join(" ");
+    if(!warnreason) return message.channel.send("PLEASE INCLUDE A REASON FOR THE WARN**!**");
+    
+    message.channel.send(`${wUser} HAS BEEN **WARNED** BECAUSE**: ${warnreason}!**`);
 
-        if(wUser.id === message.author.id) return message.channel.send("YOU CAN NOT WARN YOURSELF**!**");
-        if(wUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("THAT USER CAN'T BE WARNED**!**");
+    let warnuserEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setTimestamp()
+    .setDescription("WARN **" + snow.snowflake + "**YOU'VE BEEN WARNED IN **" + message.guild.name + "!**")
+    .addField("REASON", warnreason)
+    .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
 
-        let warnreason = args.slice(1).join(" ");
-        if(!warnreason) return message.channel.send("PLEASE INCLUDE A REASON FOR THE WARN**!**");
+    wUser.send(warnuserEmbed);
 
-        message.channel.send(`${wUser} HAS BEEN **WARNED** BECAUSE**: ${warnreason}!**`);
+    let warnEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setTimestamp()
+    .setDescription("WARN **" + snow.snowflake + "**")
+    .addField("USER", wUser)
+    .addField("MODERATOR", message.author)
+    .addField("CHANNEL", message.channel)
+    .addField("REASON", warnreason)
+    .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
 
-        warnuserEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setTimestamp()
-        .setDescription("WARN **❆**\nYOU'VE BEEN WARNED IN **" + message.guild.name + "!**")
-        .addField("REASON", warnreason)
-        .setFooter("SNOW ❆", bot.user.displayAvatarURL);
+    let snowlog = message.guild.channels.find(`name`, "snow");
+    if(!snowlog) return;
 
-        wUser.send(warnuserEmbed);
-
-        let warnEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setTimestamp()
-        .setDescription("WARN **❆**")
-        .addField("USER", wUser)
-        .addField("MODERATOR", message.author)
-        .addField("CHANNEL", message.channel)
-        .addField("REASON", warnreason)
-        .setFooter("SNOW ❆", bot.user.displayAvatarURL);
-
-        let snowlog = message.guild.channels.find(`name`, "snow");
-        if(!snowlog) return;
-
-        snowlog.send(warnEmbed);
-
-    }
+    snowlog.send(warnEmbed);
 
 }
 
