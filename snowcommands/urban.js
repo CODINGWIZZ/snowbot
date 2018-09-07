075ae237cca8081379d815d8ad009481c5e6c9ed
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:45:33 +0200

    URBAN

--- snowcommands/urban.js
+++ snowcommands/urban.js
@@ -3,39 +3,33 @@ const snow = require("../snow.json");
 
 const urban = require("relevant-urban");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
+    let urbansearch = args.join(" ");
+    if(!urbansearch) return message.channel.send("PLEASE ENTER AN ARGUMENT TO SEARCH FOR IN THE URBAN DICTIONARY DATABASE**!**");
 
-    if(cmd === `${prefix}urban`) {
+    let res = await urban(urbansearch).catch(e => {
 
-        let urbansearch = args.join(" ");
-        if(!urbansearch) return message.channel.send("PLEASE ENTER AN ARGUMENT TO SEARCH FOR IN THE URBAN DICTIONARY DATABASE**!**");
+        return message.channel.send("COULDN'T FIND THAT WORLD IN THE URBAN DICTIONARY DATABASE**!**");
+        return;
 
-        let res = await urban(urbansearch).catch(e => {
+    });
 
-            return message.channel.send("COULDN'T FIND THAT WORD IN THE URBAN DICTIONARY DATABASE**!**");
-            return;
+    message.channel.send("SEARCHING IN THE URBAN DICTIONARY DATABASE **...**").then((urbanMessage) => {
 
-        });
-        
-        message.channel.send("SEARCHING IN THE URBAN DICTIONARY DATABASE **...**").then((urbanMessage) => {
+        let urbanEmbed = new Discord.RichEmbed()
+        .setColor(snow.blue)
+        .setDescription("URBAN DICTIONARY **" + snow.snowflake + "\n" + res.word + "**\n\n**DEFINITION:**\n" + `${res.definition}\n\n**EXAMPLE:**\n${res.example}`)
+        .addField("UPVOTES [⇑]", res.thumbsUp)
+        .addField("DOWNVOTES [⇓]", res.thumbsDown)
+        .addField("WRITTEN BY", res.author)
+        .setFooter("URBAN DICTIONARY | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
 
-            let urbanEmbed = new Discord.RichEmbed()
-            .setColor(snow.blue)
-            .setDescription("URBAN DICTIONARY **❆\n" + res.word + "**\n\n**DEFINITION:**\n" + `${res.definition}\n\n**EXAMPLE:**\n${res.example}`)
-            .addField("UPVOTES [⇑]", res.thumbsUp)
-            .addField("DOWNVOTES [⇓]", res.thumbsDown)
-            .addField("WRITTEN BY", res.author)
-            .setFooter("URBAN DICTIONARY | SNOW ❆", bot.user.displayAvatarURL);
+        urbanMessage.edit(urbanMessage);
 
-            urbanMessage.edit(urbanEmbed);
-
-        });
-
-    }
+    });
 
 }
 
