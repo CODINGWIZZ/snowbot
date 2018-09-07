4e245e83881faf68155e0acbab0b91f5a80152b2
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:42:06 +0200

    SAY

--- snowcommands/say.js
+++ snowcommands/say.js
@@ -1,23 +1,17 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
-    
-    if(cmd === `${prefix}say`) {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
 
-        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    let sayMessage = args.join(" ");
+    if(!sayMessage) return message.channel.send("PLEASE ENTER A MESSAGE YOU WANT TO SAY TROUGH **SNOW!**");
 
-        let sayMessage = args.join(" ");
-        if(!sayMessage) return message.channel.send("PLEASE ENTER A MESSAGE YOU WANT TO SAY THROUGH THE BOT**!**");
-
-        message.delete();
-        message.channel.send(`**${message.author.username}** SAID THROUGH THE BOT**:**\n\n${sayMessage}`);
-
-    }
+    message.delete();
+    message.channel.send(`**${message.author.username}** SAID THROUGH **SNOW:**\n\n${sayMessage}`);
 
 }
 
