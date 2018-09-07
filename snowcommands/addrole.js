480faf849db6b9d4ca98985070b53aeb8aa394f9
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:06:23 +0200

    ADDROLE

--- snowcommands/addrole.js
+++ snowcommands/addrole.js
@@ -1,52 +1,46 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
+let prefix = snow.prefix;
+
 module.exports.run = async (bot, message, args) => {
 
-    let prefix = snow.prefix;
-    let messageArray = message.content.split(" ");
-    let cmd = messageArray[0].toLocaleLowerCase();
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
 
-    if(cmd === `${prefix}addrole`) {
+    if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO ADD A ROLE TO**!**");
 
-        if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    let arUser = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
+    if(!arUser) return message.channel.send("CAN'T FIND USER**!**");
 
-        if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO ADD A ROLE TO**!**");
+    let role = args.slice(1).join(" ");
+    if(!role) return message.channel.send("PLEASE SPECIFY A ROLE**!**");
 
-        let arUser = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
-        if(!arUser) return message.channel.send("CAN'T FIND USER**!**");
+    let arRole = message.guild.roles.find(`name`, role);
+    if(!arRole) return message.channel.send("CAN'T FIND ROLE**!**");
 
-        let role = args.slice(1).join(" ");
-        if(!role) return message.channel.send("PLEASE SPECIFY A ROLE**!**");
+    if(arUser.highestRole.position >= message.member.higestRole.position) return message.channel.send("YOU CAN NOT ADD A ROLE TO A MEMBER WITH A HIGHER OR THE SAME ROLE AS YOU**!**");
+    if(arUser.roles.has(arRole.id)) return message.channel.send("THAT USER ALREADY HAS THAT ROLE**!**");
 
-        let arRole = message.guild.roles.find(`name`, role);
-        if(!arRole) return message.channel.send("CAN'T FIND ROLE**!**");
+    await(arUser.addRole(arRole.id)).then(() => {
 
-        if(arUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT ADD A ROLE TO A MEMBER WITH A HIGHER OR THE SAME ROLE AS YOU**!**");
-        if(arUser.roles.has(arRole.id)) return message.channel.send("THAT USER ALREADY HAVE THAT ROLE**!**");
+        message.channel.send(`<@${arUser.id}> HAS BEEN ADDED TO THE **${arRole}** ROLE**!**`);
 
-        await(arUser.addRole(arRole.id)).then(() => {
+        let addroleEmbed = new Discord.RichEmbed()
+        .setColor(snow.blue)
+        .setDescription("ADD ROLE " + snow.snowflake + "**")
+        .setTimestamp()
+        .addField("USER", arUser)
+        .addField("ROLE", arRole)
+        .addField("MODERATOR", message.author)
+        .addField("CHANNEL", message.channel)
+        .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
 
-            message.channel.send(`<@${arUser.id}> HAS BEEN ADDED TO THE **${arRole}** ROLE**!**`);
+        let snowlog = message.guild.channels.find(`name`, "snow");
+        if(!snowlog) return;
 
-            let addroleEmbed = new Discord.RichEmbed()
-            .setColor(snow.blue)
-            .setDescription("ADD ROLE **❆**")
-            .setTimestamp()
-            .addField("USER", arUser)
-            .addField("ROLE", arRole)
-            .addField("MODERATOR", message.author)
-            .addField("CHANNEL", message.channel)
-            .setFooter("SNOW ❆", bot.user.displayAvatarURL);
+        snowlog.send(addroleEmbed);
 
-            let snowlog = message.guild.channels.find(`name`, "snow");
-            if(!snowlog) return;
-
-            snowlog.send(addroleEmbed);
-
-        });
-
-    }
+    });
 
 }
 
