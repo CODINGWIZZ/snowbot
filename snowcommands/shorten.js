e6d41d1b20080131dc56b5543c658cacdb968769
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-07 15:43:14 +0200

    SHORTEN

--- snowcommands/shorten.js
+++ snowcommands/shorten.js
@@ -1,35 +1,29 @@
-    const Discord = require("discord.js");
-    const snow = require("../snow.json");
+const Discord = require("discord.js");
+const snow = require("../snow.json");
 
-    const shorten = require("isgd");
+const shorten = require("isgd");
 
-    module.exports.run = async (bot, message, args) => {
+let prefix = snow.prefix;
 
-        let prefix = snow.prefix;
-        let messageArray = message.content.split(" ");
-        let cmd = messageArray[0].toLocaleLowerCase();
+module.exports.run = async (bot, message, args) => {
 
-        if(cmd === `${prefix}shorten`) {
+    let link = args[0];
+    if(!link) return message.channel.send("PLEASE ENTER A LINK THAT YOU WANT TO SHORTEN**!**");
 
-            let link = args[0];
-            if(!link) return message.channel.send("PLEASE ENTER A LINK THAT YOU WANT TO SHORTEN**!**");
+    shorten.shorten(link, function(res) {
 
-            shorten.shorten(link, function(res) {
+        message.channel.send("SHORTENING LINK **...**").then((shortenMessage) => {
 
-                message.channel.send("SHORTEN LINK **...**").then((shortenMessage) => {
+            if(res.startsWith("Error:")) return message.channel.send("PLEASE ENTER A VALID LINK**!**");
 
-                    if(res.startsWith("Error:")) return shortenMessage.edit("PLEASE ENTER A VALID LINK**!**");
+            shortenMessage.edit("**FINISHED!**\n" + `<${res}>`);
 
-                    shortenMessage.edit("**FINISHED!**\n" + `<${res}>`);
+        });
 
-                });
+    });
 
-            });
-    
-        }
+}
 
-    }
-
-    module.exports.help = {
-        name: "shorten"
-    }
+module.exports.help = {
+    name: "shorten"
+}
