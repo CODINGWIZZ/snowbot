60d14655a921a50d3cccce183144d0bf83129a91
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-21 19:50:07 +0200

    RANDOMCOLOR

--- snowcommands/randomcolor.js
+++ snowcommands/randomcolor.js
@@ -7,11 +7,11 @@ let prefix = snow.prefix;
 
 module.exports.run = async (bot, message, args) => {
 
-    let color = "000000".replace(/0/g,function(){return (~~~(Math.random()*16)).toString(16);}).toLocaleLowerCase();
+    let color = "#000000".replace(/0/g,function(){return (~~~(Math.random()*16)).toString(16);}).toLocaleLowerCase();
 
     let randomcolorEmbed = new Discord.RichEmbed()
     .setColor(snow.blue)
-    .setAuthor(`#${color.toUpperCase()}`, `https://dummyimage.com/250/${color}/&text=%20`)
+    .setAuthor(`${color.toUpperCase()}`, `https://dummyimage.com/250/${color}/&text=%20`)
     .setDescription(`[[**INFORMATION**]](https://colorhexa.com/${color})` + "\n\n" + `**RGB**\n${convert.hex.rgb(color)}\n\n**HSL**\n${convert.hex.hsl(color)}\n\n**CSS**\n${convert.hex.keyword(color).toUpperCase()}`)
     .setFooter("RANDOM COLOR | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
 
