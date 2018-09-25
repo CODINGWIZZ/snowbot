a57cebaf67f3893fe862c518d367ef0409194826
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-25 09:25:11 +0200

    DATE

--- snowcommands/date.js
+++ snowcommands/date.js
@@ -6,19 +6,12 @@ let prefix = snow.prefix;
 module.exports.run = async (bot, message, args) => {
 
     let date = new Date();
-    
-    var objToday = new Date(),
-	    weekday = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
-	    dayOfWeek = weekday[objToday.getDay()],
-	    domEnder = function() { var a = objToday; if (/1/.test(parseInt((a + "").charAt(0)))) return "th"; a = parseInt((a + "").charAt(1)); return 1 == a ? "st" : 2 == a ? "nd" : 3 == a ? "rd" : "th" }(),
-	    dayOfMonth = today + ( objToday.getDate() < 10) ? '0' + objToday.getDate() + domEnder : objToday.getDate() + domEnder,
-	    months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'),
-	    curMonth = months[objToday.getMonth()],
-	    curYear = objToday.getFullYear()
+
+    let d = (new Date()).toString().split(' ').splice(1,3).join(' ');
 
     let dateEmbed = new Discord.RichEmbed()
     .setColor(snow.blue)
-    .setDescription(dayOfWeek + ", " + dayOfMonth.toUpperCase() + " " + curMonth.toUpperCase() + ", " + curYear + " **" + date.toUTCString().slice(16) + "**");
+    .setDescription(d.toUpperCase() + " **" + date.toUTCString().slice(16) + "**");
 
     message.channel.send(dateEmbed);
 
