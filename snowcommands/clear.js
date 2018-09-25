7e504ff4fc3098e6e534ef0a6d8f0ead79f8156c
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-09-25 18:39:11 +0200

    CLEAR

---
+++ snowcommands/clear.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+module.exports.run = async (bot, message, args) => {
+
+    message.delete().catch(O_o=>{});
+    
+    const amount = isNaN(args[0]) ? parseInt(args[1]) : parseInt(args[0]);
+    
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    if(amount < 1) return message.channel.send("CANNOT DELETE A NEGATIVE NUMBER OF MESSAGES**!**");
+    if(amount > 99) return message.channel.send("CAN ONLY DELETE 99 MESSAGES MAX**!**");
+    
+    if(!args[0]) return message.channel.send("PLEASE ENTER A CERTAN NUMBER YOU WANT TO DELETE**!**");
+    
+    message.channel.bulkDelete(args[0]).then(() => {
+        message.channel.send(`CLEARED **${args[0]}** MESSAGES**!**`).then(deleteMessage => deleteMessage.delete(5000));
+    });
+    
+    return;
+
+}
+
+module.exports.help = {
+    name: "clear"
+}
