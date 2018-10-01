51730ca3da86ee53a617937c3ef471fc687fa5a6
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-10-01 14:05:42 +0200

    WIKIPEDIA

--- snowcommands/wikipedia.js
+++ snowcommands/wikipedia.js
@@ -5,8 +5,7 @@ const wiki = require("wikipediajs");
 
 module.exports.run = async (bot, message, args) => {
 
-    let wikipedia = args
-    .join(" ");
+    let wikipedia = args.join(" ");
     if(!wikipedia) return message.channel.send("PLEASE ENTER A WIKI SEARCH TERM**!**");
     
     wiki.search(wikipedia).then((res) => {
