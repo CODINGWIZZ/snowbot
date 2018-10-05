d8db6818beaa10a4a4a836a194700e4a01dfc715
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-10-05 13:51:29 +0200

    URBAN

--- snowcommands/urban.js
+++ snowcommands/urban.js
@@ -19,7 +19,7 @@ module.exports.run = async (bot, message, args) => {
 
     message.channel.send("SEARCHING IN THE URBAN DICTIONARY DATABASE **...**").then((urbanMessage) => {
         
-        if(urbanMessage.startsWith("RangeError:")) return message.channel.send("THE WORD YOU WANT TO SEARCH IN THE URBAN DICTIONARY LIBRARY IS TO LONG FOR THE EMBED TO HANDLE**!**");
+        if(console.log.startsWith("RangeError:")) return message.channel.send("THE WORD YOU WANT TO SEARCH IN THE URBAN DICTIONARY LIBRARY IS TO LONG FOR THE EMBED TO HANDLE**!**");
 
         let urbanEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
