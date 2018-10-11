ce4f9d9d0aa6f13febd5aabf8d2f9049138a9a35
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-10-11 10:24:31 +0200

    SNOW

--- snow.js
+++ snow.js
@@ -284,13 +284,37 @@ bot.on("message", async message => {
     
     if(cmd === `${prefix}skolmat`) {
      
-        let res = await got(`https://skolmaten.se/stavsborgsskolan/rss/weeks/`);
+    $(document).ready(function() {
+	//feed to parse
+	var feed = "https://skolmaten.se/stavsborgsskolan/rss/weeks/";
+	
+	$.ajax(feed, {
+		accepts:{
+			xml:"application/rss+xml"
+		},
+		dataType:"xml",
+		success:function(data) {
+			//Credit: http://stackoverflow.com/questions/10943544/how-to-parse-an-rss-feed-using-javascript
+
+			$(data).find("item").each(function () { // or "item" or whatever suits your feed
+				var el = $(this);
+				console.log("------------------------");
+				console.log("title      : " + el.find("title").text());
+				console.log("link       : " + el.find("link").text());
+				console.log("description: " + el.find("description").text());
+			});
+	
+
+		}	
+	});
+	
+});
         
-        let skolmatEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setDescription(res.title);
+//         let skolmatEmbed = new Discord.RichEmbed()
+//         .setColor(snow.blue)
+//         .setDescription(res.title);
         
-        message.channel.send(skolmatEmbed);
+//         message.channel.send(skolmatEmbed);
         
     }
 
