a8b6cf0b5c18ca20203f273d2abfc70ef2527766
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-10-22 20:23:11 +0200

    FORECAST

--- snowcommands/forecast.js
+++ snowcommands/forecast.js
@@ -9,20 +9,20 @@ let prefix = snow.prefix;
 
 module.exports.run = async (bot, message, args) => {
 
-    const location = args.join(" ");
-    if(!location) return message.channel.send("PLEASE ENTER A CITY OR A ZIP CODE THAT YOU WANT TO CHECK FORECAST FOR**!**");
+    let location = args.join(" ");
+    if(!location) return message.channel.send("PLEASE ENTER A CITY OR A ZIP CODE THAT YOU WANT TO CHECK FORECAST ABOUT**!**");
 
-    let makeURL = (location) => `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22$%7B$%7BencodeURIComponent(location)%7D%7D%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
-    const res = await got(makeURL(location), { json: true });
+    let locationURL = location => `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22$%7B${encodeURIComponent(location)}%7D%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+    let res = await got(locationURL(location), { json: true });
 
-    const weatherinfo = res.body.query.results.channel;
-    const forecastday = weatherinfo.item.forecast[0];
+    let weatherinfo = res.body.query.results.channel;
+    let forecast = weatherinfo.item.forecast[0];
 
     weather.find({search: location, degreeType: "F"}, function(err, result) {
 
         message.channel.send("GENERATING FORECAST INFORMATAION **...**").then((forecastMessage) => {
 
-            if(!res || !res.body.query || !res.body.query.results || !res.body.query.results.channel) {
+            if(!res || !res.body || !res.body.query || !res.body.query.results || !res.body.query.results.channel) {
 
                 return forecastMessage.edit("COULDN'T FIND FORECAST**!**");
 
@@ -31,12 +31,12 @@ module.exports.run = async (bot, message, args) => {
             const countryinfo = countries.find(country => country.name === weatherinfo.location.country);
             const countryemoji = countryinfo ? countryinfo.emoji : "** **";
 
-            const celsius = (fahrenheit) => Math.round(((fahrenheit - 32) * 5) / 9);
+            let celsius = (fahrenheit) => Math.round(((fahrenheit - 32) * 5) / 9);
 
             let forecastEmbed = new Discord.RichEmbed()
             .setColor(snow.blue)
             .setTimestamp()
-            .setDescription("FORECAST :cloud:\n" + `${countryemoji} **//** ${result[0].location.name}\n\n` + "**" + result[0].forecast[1].day + "**\nLOW**:** " + celsius(result[0].forecast[1].low) + "**°C // ** "+ result[0].forecast[1].low + "**°F**\nHIGH**:** " + celsius(result[0].forecast[1].high) + "**°C // **" + result[0].forecast[1].high + "**°F**\n`" + result[0].forecast[1].skytextday + "`\n\n" + "**" + result[0].forecast[2].day + "**\nLOW**:** " + celsius(result[0].forecast[2].low) + "**°C // **" + result[0].forecast[2].low + "**°F**\nHIGH**:** " + celsius(result[0].forecast[2].high) + "**°C // **" + result[0].forecast[2].high + "**°F**\n`" + result[0].forecast[2].skytextday + "`\n\n**" + result[0].forecast[3].day + "**\nLOW**:** " + celsius(result[0].forecast[3].low) + "**°C // **" + result[0].forecast[3].low + "**°F**\nHIGH**:** " + celsius(result[0].forecast[3].high) + "**°C // **" + result[0].forecast[3].high + "**°F**\n`" + result[0].forecast[3].skytextday + "`\n\n**" + result[0].forecast[4].day + "**\nLOW**:** " + celsius(result[0].forecast[4].low) + "**°C // **" + result[0].forecast[4].low +"**°F**\nHIGH**:** " + celsius(result[0].forecast[4].high) + "**°C // **" + result[0].forecast[4].high + "**°F**\n`" + result[0].forecast[4].skytextday + "`")
+            .setDescription("FORECAST :cloud:\n" + `${countryemoji} **//** ${result[0].location.name}\n\n` + "**" + result[0].forecast[1].day + "**\nLOW**:** " + celsius(result[0].forecast[1].low) + "**°C // ** "+ result[0].forecast[1].low + "**°F**\nHIGH**:** " + celsius(result[0].forecast[1].high) + "**°C // **" + result[0].forecast[1].high + "**°F**\n`" + result[0].forecast[1].skytextday.toUpperCase() + "`\n\n" + "**" + result[0].forecast[2].day + "**\nLOW**:** " + celsius(result[0].forecast[2].low) + "**°C // **" + result[0].forecast[2].low + "**°F**\nHIGH**:** " + celsius(result[0].forecast[2].high) + "**°C // **" + result[0].forecast[2].high + "**°F**\n`" + result[0].forecast[2].skytextday.toUpperCase() + "`\n\n**" + result[0].forecast[3].day + "**\nLOW**:** " + celsius(result[0].forecast[3].low) + "**°C // **" + result[0].forecast[3].low + "**°F**\nHIGH**:** " + celsius(result[0].forecast[3].high) + "**°C // **" + result[0].forecast[3].high + "**°F**\n`" + result[0].forecast[3].skytextday.toUpperCase() + "`\n\n**" + result[0].forecast[4].day + "**\nLOW**:** " + celsius(result[0].forecast[4].low) + "**°C // **" + result[0].forecast[4].low +"**°F**\nHIGH**:** " + celsius(result[0].forecast[4].high) + "**°C // **" + result[0].forecast[4].high + "**°F**\n`" + result[0].forecast[4].skytextday.toUpperCase() + "`")
             .setFooter("FORECAST | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
 
             forecastMessage.edit(forecastEmbed);
