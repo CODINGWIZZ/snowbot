ce0ce86d6cee4fc1a699df7e4188c272000af41f
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-02 20:44:56 +0100

    DATE

--- snowcommands/date.js
+++ snowcommands/date.js
@@ -6,14 +6,14 @@ let prefix = snow.prefix;
 module.exports.run = async (bot, message, args) => {
 
     let date = new Date(),
-        weekdays = new Array('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'),
+        weekdays = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"),
         weekday = weekdays[date.getDay()];
 
-    let d = (new Date()).toString().split(' ').splice(1,3).join(' ');
+    let d = (new Date()).toString().split(" ").splice(1,3).join(" ");
 
     let dateEmbed = new Discord.RichEmbed()
     .setColor(snow.blue)
-    .setDescription(weekday.toUpperCase() + " " + d.toUpperCase() + " **" + date.toUTCString().slice(16).replace("GMT", "ZULU") + "**");
+    .setDescription(weekday.toUpperCase() + " " + d.toUpperCase() + " **" + date.toUTCString().slice(16) + "**");
 
     message.channel.send(dateEmbed);
 
