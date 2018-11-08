dc6776d53afa15b54035052e126b0c5e32249de9
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-08 12:25:07 +0100

    COLOR

--- snowcommands/color.js
+++ snowcommands/color.js
@@ -7,9 +7,8 @@ let prefix = snow.prefix;
 
 module.exports.run = async (bot, message, args) => {
 
+    if(!args[0]) return message.channel.send("PLEASE ENTER A HEX COLOR YOU WANT TO CHECK**!**");
     let color = args[0].replace("#", "");
-        
-    if(!color) return message.channel.send("PLEASE ENTER A HEX COLOR THAT YOU WANT TO CHECK**!**");
 
     if(color.length > 6) return message.channel.send("PLEASE ENTER A VALID HEX CODE**!**");
     if(color.length < 6) return message.channel.send("PLEASE ENTAR A VALID HEX COLOR**!**");
