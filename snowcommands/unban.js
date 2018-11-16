9f8f21f3cde9d36689f538ce7e32f5bf6a14fd08
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-17 00:24:09 +0100

    UNBAN

--- snowcommands/unban.js
+++ snowcommands/unban.js
@@ -5,17 +5,16 @@ module.exports.run = async (bot, message, args) => {
 
     if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
     
-    let userid = args[0];
-    if(!userid) return message.channel.send("PLEASE ENTER A USER ID OF A USER YOU WANT TO UNBAN AND THEN A REASON**!**");
-    if(userid.length !== 18) return message.channel.send("THAT'S NOT AN VALID ID**!**");
+    let user = args[0];
+    if(!user) return message.channel.send("PLEASE ENTER A USER RESOLVABLE, LIKE A ID OF THE USER YOU WANT TO UNBAN AND THEN A REASON**!**");
     
     if(userid === message.author.id) return message.channel.send("YOU CAN'T UNBAN YOURSELF FROM A SERVER THAT YOU ALREADY ARE IN**!**");
     
     let unbanreason = args.slice(1).join(" ");
     if(!unbanreason) return message.channel.send("PLEASE ENTER A REASON FOR THE UNBAN**!**");
     
-    guild.unban(userid)
-        .then(user => message.channel.send("**" + user.username + "** HAS BEEN **UNBANNED** FROM **" + guild + "!**"));
+    message.guild.unban(user)
+        .then(user => message.channel.send("**" + user.username + "** HAS BEEN **UNBANNED** FROM **" + message.guild.name + "!**"));
     
 }
 
