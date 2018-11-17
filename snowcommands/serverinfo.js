2ab399fd19262113261740c7469e4fb504141ebc
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-17 22:08:26 +0100

    SERVERINFO

--- snowcommands/serverinfo.js
+++ snowcommands/serverinfo.js
@@ -66,10 +66,10 @@ module.exports.run = async (bot, message, args) => {
     .addField("VERTIFICATION LEVEL", vertification[message.guild.verificationLevel])
     .addField("CREATED", message.guild.createdAt.toDateString().toUpperCase(), true)
     .addField("REGION", message.guild.region.toUpperCase(), true)
-    .addField("CHANNELS", message.guild.channels.size + "\n\n **TEXT CHANNELS:** " + message.guild.channels.filter(tc => tc.type === "text").size + "\n **VOICE CHANNELS:** " + message.guild.channels.filter(vc => vc.type === "voice").size, true)
     .addField("TOTAL ROLES", message.guild.roles.size, true)
     .addField("TOTAL MEMBERS", message.guild.memberCount, true)
     .addField("ONLINE MEMBERS", onlinemembers + idlemembers + dndmembers, true)
+    .addField("CHANNELS", message.guild.channels.size + "\n\n **TEXT CHANNELS:** " + message.guild.channels.filter(tc => tc.type === "text").size + "\n **VOICE CHANNELS:** " + message.guild.channels.filter(vc => vc.type === "voice").size, true)
     .addField("CURRENT MEMBER PRESENCES", snowonline + " **//** " + onlinemembers + "\n" + snowidle + " **//** " + idlemembers + "\n" + snowdnd + " **//** " + dndmembers + "\n" + snowoffline + " **//** " + offlinemembers)
     .setFooter("SERVER INFO | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
 
