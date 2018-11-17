d59155276de70b8f12dfe8ea89cef59768b85f28
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-17 21:51:58 +0100

    CAT

--- snowcommands/cat.js
+++ snowcommands/cat.js
@@ -8,14 +8,14 @@ let prefix = snow.prefix;
 module.exports.run = async (bot, message, args) => {
 
     const { body, header } = await superagent
-    .get("https://aws.random.cat/meow");
+    .get("http://thecatapi.com/api/images/get?format=xml");
 
     message.channel.send("GENERATING CAT IMAGE **...**").then((catMessage) => {
 
         let catEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
         .setDescription("CAT **" + snow.snowflake + "**")
-        .setImage(body.file);
+        .setImage(body.url);
 
 
         catMessage.edit(catEmbed);
