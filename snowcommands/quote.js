82696a4df62f8b545f5d9e1bbb7c4214196cc2ec
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-17 22:55:03 +0100

    QUOTE

---
+++ snowcommands/quote.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+const fetch = require("snekfetch");
+
+module.exports.run = async(bot, message, args) => {
+
+    fetch.get("http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en").then((thequote => {
+    
+        if(thequote.body.quoteText === undefined) {
+        
+            return message.channel.send("COULDN'T LOAD THE QUOTE**!**");
+        
+        }
+        
+        let quoteEmbed = new Discord.RichEmbed()
+        .setColor(snow.blue)
+        .setDescription([**snow.snowflake** + " "](thequote.body.quoteLink) + thequote.body.quoteText + "\n\n" + "**-" + thequote.body.quoteAuthor + "**")
+        .setFooter("QUOTE | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+        
+        message.channel.send(quoteEmbed);
+    
+    });
+
+}
+
+module.exports.help = {
+    name: "quote"
+}
