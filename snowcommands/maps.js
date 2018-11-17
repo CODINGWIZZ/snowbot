d759f34a2228b8fd0dda14e593f5219323908cf2
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-17 23:51:16 +0100

    MAPS

--- snowcommands/maps.js
+++ snowcommands/maps.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
     let maps = encode(args.join(" "));
     if(!maps) return message.channel.send("PLEASE ENTER A SEARCH QUERY YOU WANT TO SEARCH FOR IN GOOGLE MAPS**!**");
     
-    let mapslink = `https://maps.google.com/?q=${maps}`;
+    let mapslink = `https://google.com/maps?q=${maps}`;
     
     message.channel.send("SEARCHING FOR RESULTS IN GOOGLE MAPS **...**").then((mapsMessage) => {
     
