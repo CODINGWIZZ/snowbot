33f8c91c96467f70f0d03c9120399043fe4a2d07
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-24 00:07:21 +0100

    CLEAR

--- snowcommands/clear.js
+++ snowcommands/clear.js
@@ -3,52 +3,54 @@ const snow = require("../snow.json");
 
 module.exports.run = async (bot, message, args) => {
 
-    message.delete();
+    message.delete().then(() => {
     
-    const amount = isNaN(args[0]) ? parseInt(args[1]) : parseInt(args[0]);
-    if(!amount) return message.channel.send("PLEASE ENTER A CERTAN NUMBER YOU WANT TO DELETE**!**");
+        const amount = isNaN(args[0]) ? parseInt(args[1]) : parseInt(args[0]);
+        if(!amount) return message.channel.send("PLEASE ENTER A CERTAN NUMBER YOU WANT TO DELETE**!**");
     
-    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
-    if(amount < 0) return message.channel.send("CANNOT DELETE A NEGATIVE NUMBER OF MESSAGES**!**");
-    if(amount > 99) return message.channel.send("CAN ONLY DELETE **99** MESSAGES MAX**!**");
-    if(amount === 0) {
+        if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+        if(amount < 0) return message.channel.send("CANNOT DELETE A NEGATIVE NUMBER OF MESSAGES**!**");
+        if(amount > 99) return message.channel.send("CAN ONLY DELETE **99** MESSAGES MAX**!**");
+        if(amount === 0) {
     
-        message.channel.bulkDelete("99").then(() => {
-            message.channel.send(`CLEARED **99** MESSAGES**!**`).then(deleteMessage => deleteMessage.delete(5000));
+            message.channel.bulkDelete("99").then(() => {
+                message.channel.send(`CLEARED **99** MESSAGES**!**`).then(deleteMessage => deleteMessage.delete(5000));
             
-            let clearallEmbed = new Discord.RichEmbed()
-            .setColor(snow.blue)
-            .setDescription("CLEAR **" + snow.snowflake + "**")
-            .addField("AMOUNT", "`99`")
-            .addField("MODERTOR", message.author)
-            .addField("CHANNEL", message.channel)
-            .setFooter("CLEAR | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+                let clearallEmbed = new Discord.RichEmbed()
+                .setColor(snow.blue)
+                .setDescription("CLEAR **" + snow.snowflake + "**")
+                .addField("AMOUNT", "`99`")
+                .addField("MODERTOR", message.author)
+                .addField("CHANNEL", message.channel)
+                .setFooter("CLEAR | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
         
-            let snowlog = message.guild.channels.find(`name`, "snow");
-            if(!snowlog) return;
+                let snowlog = message.guild.channels.find(`name`, "snow");
+                if(!snowlog) return;
             
-            snowlog.send(clearallEmbed);
+                snowlog.send(clearallEmbed);
             
-        });
+            });
        
-    }
+        }
     
-    message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(`CLEARED **${args[0]}** MESSAGES**!**`).then(deleteMessage2 => deleteMessage2.delete(5000));
+        message.channel.bulkDelete(args[0]).then(() => {
+            message.channel.send(`CLEARED **${args[0]}** MESSAGES**!**`).then(deleteMessage2 => deleteMessage2.delete(5000));
+        });
+    
+        let clearEmbed = new Discord.RichEmbed()
+        .setColor(snow.blue)
+        .setDescription("CLEAR **" + snow.snowflake + "**")
+        .addField("AMOUNT", "`" + args[0] + "`")
+        .addField("MODERATOR", message.author)
+        .addField("CHANNEL", message.channel)
+        .setFooter("CLEAR | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+    
+        let snowlog1 = message.guild.channels.find(`name`, "snow");
+        if(!snowlog1) return;
+    
+        snowlog1.send(clearEmbed);
+        
     });
-    
-    let clearEmbed = new Discord.RichEmbed()
-    .setColor(snow.blue)
-    .setDescription("CLEAR **" + snow.snowflake + "**")
-    .addField("AMOUNT", "`" + args[0] + "`")
-    .addField("MODERATOR", message.author)
-    .addField("CHANNEL", message.channel)
-    .setFooter("CLEAR | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
-    
-    let snowlog1 = message.guild.channels.find(`name`, "snow");
-    if(!snowlog1) return;
-    
-    snowlog1.send(clearEmbed);
 
 }
 
