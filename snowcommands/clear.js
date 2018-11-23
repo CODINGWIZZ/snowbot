7fe0b5d8963b4257a3df8e53ea8061b16dbfc54e
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-24 00:03:23 +0100

    CLEAR

--- snowcommands/clear.js
+++ snowcommands/clear.js
@@ -6,6 +6,7 @@ module.exports.run = async (bot, message, args) => {
     message.delete().catch(O_o=>{});
     
     const amount = isNaN(args[0]) ? parseInt(args[1]) : parseInt(args[0]);
+    if(!amount) return message.channel.send("PLEASE ENTER A CERTAN NUMBER YOU WANT TO DELETE**!**");
     
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
     if(amount < 0) return message.channel.send("CANNOT DELETE A NEGATIVE NUMBER OF MESSAGES**!**");
@@ -13,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
     if(amount === 0) {
     
         message.channel.bulkDelete("99").then(() => {
-            message.channel.send(`CLEARED **99** MESSAGES**!**`).then(deleteMessage2 => deleteMessage2.delete(5000));
+            message.channel.send(`CLEARED **99** MESSAGES**!**`).then(deleteMessage => deleteMessage.delete(5000));
             
             let clearallEmbed = new Discord.RichEmbed()
             .setColor(snow.blue)
@@ -32,10 +33,8 @@ module.exports.run = async (bot, message, args) => {
        
     }
     
-    if(!args[0]) return message.channel.send("PLEASE ENTER A CERTAN NUMBER YOU WANT TO DELETE**!**");
-    
     message.channel.bulkDelete(args[0]).then(() => {
-        message.channel.send(`CLEARED **${args[0]}** MESSAGES**!**`).then(deleteMessage => deleteMessage.delete(5000));
+        message.channel.send(`CLEARED **${args[0]}** MESSAGES**!**`).then(deleteMessage2 => deleteMessage2.delete(5000));
     });
     
     let clearEmbed = new Discord.RichEmbed()
