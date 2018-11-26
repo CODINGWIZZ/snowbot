280a395f063f6c3b2d370dc8c6e41c1a4c9d14ce
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-26 22:00:34 +0100

    SNOW

--- snow.js
+++ snow.js
@@ -276,7 +276,7 @@ bot.on("message", async message => {
         let memoryusage = `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} \`MB\``;
         
         let averageusers = bot.users.size / bot.guilds.size;
-        let average = Math.ceil(averageusers);
+        let average = Math.round(averageusers);
 
         let snowEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
@@ -289,7 +289,7 @@ bot.on("message", async message => {
         .addField("MEMORY // COMMANDS", memoryusage + " **//** " + snow.commands)
         .addField("UPTIME", uptime)
         .addField("WEBSITE", "https://discordsnowbot.weebly.com/")
-        .addField("STATS", `**${bot.guilds.size}** SERVERS **( ${average}** AVERAGE USERS **//** GUILD **)**\n**${bot.channels.size}** CHANNELS\n**${bot.users.size}** USERS`)
+        .addField("STATS", `**${bot.guilds.size}** SERVERS **( ${average}** AVERAGE USERS **/** GUILD **)**\n**${bot.channels.size}** CHANNELS\n**${bot.users.size}** USERS`)
         .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
 
         message.channel.send(snowEmbed);
