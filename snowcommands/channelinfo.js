241622b2ec3a75fca14f404f5c9b5f909394430a
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-27 10:33:29 +0100

    CHANNELINFO

---
+++ snowcommands/channelinfo.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+module.exports.run = async(bot, message, args) => {
+
+    let channel = message.mentions.channels.first() || message.channel;
+    
+    let channeltype = "";
+    
+    if(channel.filter.type === "text") {
+    
+        channeltype = "TEXT";
+    
+    } else if(channel.filter.type === "voice") {
+    
+        channeltype = "VOICE";
+    
+    }
+    
+    let textchannel = channel.filter(tc => tc.type === "text");
+    let voicechannel = channel.filter(vc => vc.type === "")
+    
+    let channelEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setDescription("CHANNELINFO **" + snow.snowflake + "**\n" + channel)
+    .addField("CHANNELTYPE", channeltype)
+    .addField("ID", channel.id)
+    .addField("CREATED", channel.createdAt.toDateString().toUpperCase())
+    .setFooter("CHANNELINFO | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+    
+    message.channel.send(channelEmbed);
+
+}
+
+module.exports.help = {
+    name: "channelinfo"
+}
