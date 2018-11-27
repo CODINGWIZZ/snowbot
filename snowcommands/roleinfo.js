55caa2f146d7dfbc288e75b2c9ca02821df2e9f4
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-11-27 16:32:51 +0100

    ROLEINFO

--- snowcommands/roleinfo.js
+++ snowcommands/roleinfo.js
@@ -31,8 +31,8 @@ module.exports.run = async(bot, message, args) => {
     .addField("POSITION", message.guild.roles.size - role.calculatedPosition + " **/** " + message.guild.roles.size, true)
     .addField("ROLE MEMBERS", role.members.size, true)
     .addField("COLOR", "**#**" + rolecolor.toUpperCase() + "\n**RGB(**" + convert.hex.rgb(rolecolor) + "**)**")
+    .addField("MENTIONABLE // MANAGED // HOIST", truefalse[mentionable] + " **//** " + truefalse[managed] + " **//** " + truefalse[hoist], true)
     .addField("CREATED AT", role.createdAt.toDateString().toUpperCase())
-    .addField("MENTIONABLE // MANAGED // HOIST", truefalse[mentionable] + " **//** " + truefalse[managed] + " **//** " + truefalse[hoist])
     .setFooter("ROLEINFO | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
     
     message.channel.send(roleinfoEmbed);
