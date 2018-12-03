aba5d77e18cbfd193947ac904a63fe6dc7700a0e
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-03 19:49:59 +0100

    JOIN

---
+++ snowcommands/join.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+module.exports.run = async(bot, message, args) => {
+
+    let voicechannel = message.member.voiceChannel;
+    if(!voicechannel) return message.channel.send("PLEASE CONNECT TO A VOICECHANNEL**!**");
+
+    if(message.guild.me.voiceChannel) return message.channel.send("SNOW IS ALREADY CONNECTED TO A VOICE CHANNEL IN THIS SERVER**!**");
+
+    voicechannel.join();
+    message.channel.send("JOINED **" + voicechannel.name + "!**");
+
+}
+
+module.exports.help = {
+    name: "join"
+}
