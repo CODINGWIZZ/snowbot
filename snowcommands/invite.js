efe7b36fb2b14c2494a72bb831fe0b828d75b839
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-13 13:06:19 +0100

    INVITE

--- snowcommands/invite.js
+++ snowcommands/invite.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
 
     let inviteEmbed = new Discord.RichEmbed()
     .setColor(snow.blue)
-    .setDescription("INVITE SNOW [**HERE**](https://discorsnowbot.weebly.com/invite)**!**")
+    .setDescription("INVITE SNOW [**HERE**](https://discordsnowbot.weebly.com/invite)**!**")
     .setFooter("INVITE | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
     
     message.channel.send(inviteEmbed);
