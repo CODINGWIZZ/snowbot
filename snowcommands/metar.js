d578fa4b1ed95702250d4f62c77fd6a69da5530b
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-13 22:47:55 +0100

    METAR

--- snowcommands/metar.js
+++ snowcommands/metar.js
@@ -5,7 +5,7 @@ const fetch = require("snekfetch");
 
 module.exports.run = async (bot, message, args) => {
 
-    let airport = args[0];
+    let airport = args[0].toUpperCase();
     if(!airport) return message.channel.send("PLEASE ENTER A ICAO**!**");
 
     fetch.get(`https://avwx.rest/api/metar/${airport}`).then((metar) => {
