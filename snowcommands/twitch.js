861edf99d679ab0e600aeb676c463fb3f6201960
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-16 11:31:53 +0100

    TWITCH

--- snowcommands/twitch.js
+++ snowcommands/twitch.js
@@ -12,8 +12,8 @@ module.exports.run = async (bot, message, args) => {
     
         let twitchEmbed = new Discord.RichEmbed()
         .setColor("#6441a5")
-        .setAuthor(twitchstreamer.display_name, twitchstreamer.logo)
-        .setThumbnail(twitchstreamer.logo)
+        .setAuthor(twitchstreamer.body.display_name, twitchstreamer.body.logo)
+        .setThumbnail(twitchstreamer.body.logo)
 //         .setDescription([`${twitchstreamer.status}`](`${twitchstreamer.url}`))
         .setFooter("TWITCH | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
         
