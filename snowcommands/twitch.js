b318556bc255beea04acc7e81bb311cb397e4fd7
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-16 11:17:50 +0100

    TWITCH

---
+++ snowcommands/twitch.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+const fetch = require("snekfetch");
+
+module.exports.run = async (bot, message, args) => {
+
+    let streamer = args[0];
+    if(!streamer) return message.channel.send("PLEASE ENTER A TWITCH STREAMER**!**");
+    
+    fetch(https://api.twitch.tv/kraken/streams/${streamer}?client_id=${process.env.twitchclient}).then((twitch) => {
+    
+        let twitchEmbed = new Discord.RichEmbed()
+        .setColor("#6441a5")
+        .setAuthor(twitch.display_name, twitch.logo)
+        .setThumbnail(twitch.logo)
+        .setDescription([twitch.status](twitch.url))
+        .setFooter("TWITCH | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+        
+        message.channel.send(twitchEmbed);
+    
+    });
+
+}
+
+module.exports.help = {
+    name: "twitch"
+}
