3288b08e72367c11ed510980411e5338ca393875
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-17 10:56:52 +0100

    METAR

--- snowcommands/metar.js
+++ snowcommands/metar.js
@@ -8,9 +8,7 @@ module.exports.run = async (bot, message, args) => {
     let airport = args[0].toUpperCase();
     if(!airport) return message.channel.send("PLEASE ENTER A ICAO**!**");
 
-    fetch.get(`https://avwx.rest/api/metar/${airport}`).then((metar, err) => {
-      
-        if(err) return message.channel.send("CAN'T LOAD METAR**!**");
+    fetch.get(`https://avwx.rest/api/metar/${airport}`).then((metar) => {
         
         let decodeEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
