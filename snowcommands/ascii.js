ec72ecc0c4a8c705f14fbcd12449b4ca314bd8c4
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-18 17:28:21 +0100

    ASCII

--- snowcommands/ascii.js
+++ snowcommands/ascii.js
@@ -8,6 +8,7 @@ let prefix = snow.prefix;
 module.exports.run = async (bot, message, args) => {
 
     let asciimessage = args.join(" ");
+    if(asciimessage.length > 15) return message.channel.send("THE MESSAGE YOU'VE ENTERED IS TOO LONG**!**");
     if(!asciimessage) return message.channel.send("PLEASE ENTER A MESSAGE THAT YOU WANT TO CONVERT TO ASCII**!**");
 
     message.channel.send("GENERATING ASCII MESSAGE **...**").then((asciiMessage) => {
@@ -16,7 +17,7 @@ module.exports.run = async (bot, message, args) => {
 
             finished = finished.trimRight();
 
-            if(finished.length > 270) return asciiMessage.edit("THE ASCII MESSAGE YOU WANT TO DO IS TO LONG**!**");
+//             if(finished.length > 20) return asciiMessage.edit("THE ASCII MESSAGE YOU WANT TO DO IS TO LONG**!**");
 
             asciiMessage.edit(finished, {
                 code: "md"
