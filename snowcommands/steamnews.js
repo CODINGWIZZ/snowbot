3ef0a3a61faf100394b53fa36396cbd3a2b1ee0f
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-19 10:14:30 +0100

    STEAMNEWS

---
+++ snowcommands/steamnews.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const snow = require("../snow.json");
+
+const rss = require("rss-parser");
+const parser = new Parser();
+
+module.exports.run = async (bot, message, args) => {
+
+    let feed = await parser.parseURL("https://store.steampowered.com/feeds/news.xml");
+    
+    let steamnewsEmbed = new Discord.RichEmbed()
+    .setColor(snow.blue)
+    .setDescription("STEAM NEWS **" + snow.snowflake + "\n" + feed.title + "**\n" + feed.content:encoded)
+    .setFooter("STEAM NEWS | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+    
+    message.channel.send(steamnewsEmbed);
+
+}
+
+module.exports.help = {
+    name: "steamnews"
+}
