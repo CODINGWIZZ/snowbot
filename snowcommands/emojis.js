9c18d6070bf799c3a4ea38d7efd7fc0915211877
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-21 14:10:43 +0100

    EMOJIS

--- snowcommands/emojis.js
+++ snowcommands/emojis.js
@@ -4,11 +4,12 @@ const snow = require("../snow.json");
 module.exports.run = async (bot, message, args) => {
 
     let emojilist = message.guild.emojis.map(e => e.toString()).join(" ");
+    let totalemojis = message.guild.emojis.size;
 
     let emojiEmbed = new Discord.RichEmbed()
     .setColor(snow.blue)
     .setDescription("EMOJIS **" + snow.snowflake + "**\n" + emojilist)
-    .setFooter("EMOJIS | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+    .setFooter(`${totalemojis} EMOJIS | SNOW ` + snow.snowflake, bot.user.displayAvatarURL);
 
     message.channel.send(emojiEmbed);
 
