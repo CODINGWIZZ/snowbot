12c069dcaa39e32a45c2af69f8bfdac7682d0133
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-21 14:47:24 +0100

    S W

--- snow.js
+++ snow.js
@@ -126,27 +126,27 @@ bot.on("message", async message => {
     let commandfile = bot.commands.get(cmd.slice(prefix.length));
     if(commandfile) commandfile.run(bot,message,args);
     
-    let score = bot.getScore.get(message.author.id, message.guild.id);
-    if(!score) {
-        score = {
-            id: `${message.guild.id}-${message.author.id}`,
-            user: message.author.id,
-            guild: message.guild.id,
-            points: 0,
-            level: 1
+    let score; 
+    if(message.guild) {
+        
+        score = bot.getScore.get(message.author.id, message.guild.id);
+        if(!score) {
+            score = { id: `${message.guild.id}-${message.author.id}`, user: message.author.id, guild: message.guild.id, points: 0, level: 1}   
         }
+            
     }
+
     
     score.points++;
     const curLevel = Math.floor(0.1 * Math.sqrt(score.points));
     
     if(score.level < curLevel) {
-        score.level++;
-        message.channel.send("**" + message.author.username + ",** YOU HAVE LEVELED UP TO LEVEL `" + curLevel + "`**!**");
+      score.level++;
+      message.reply(`You've leveled up to level **${curLevel}**! Ain't that dandy?`);
     }
     
     bot.setScore.run(score);
-    
+
     // OWNER COMMANDS
 
     if(cmd === `${prefix}snowembed`) {
