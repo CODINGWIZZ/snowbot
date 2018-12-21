44c0803926d7aa9a9d80328702d9d7a4ea0b3a9c
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2018-12-21 19:21:37 +0100

    [DISABLE] SERVERCOUNT

--- snowcommands/servercount.js
+++ snowcommands/servercount.js
@@ -1,82 +1,82 @@
-const Discord = require("discord.js");
-const snow = require("../snow.json");
+// const Discord = require("discord.js");
+// const snow = require("../snow.json");
 
-module.exports.run = async (bot, message, args) => {
+// module.exports.run = async (bot, message, args) => {
     
-    function checkBots(guild) {
+//     function checkBots(guild) {
         
-        let botCount = 0;
-        guild.members.forEach(member => {
+//         let botCount = 0;
+//         guild.members.forEach(member => {
             
-            if(member.user.bot) botCount ++;
+//             if(member.user.bot) botCount ++;
             
-        });
+//         });
         
-        return botCount;
+//         return botCount;
         
-    }
+//     }
     
-    function checkMembers(guild) {
+//     function checkMembers(guild) {
      
-        let memberCount = 0;
-        guild.members.forEach(member => {
+//         let memberCount = 0;
+//         guild.members.forEach(member => {
             
-            if(!member.user.bot) memberCount ++;
+//             if(!member.user.bot) memberCount ++;
             
-        });
+//         });
         
-        return memberCount;
+//         return memberCount;
         
-    }
+//     }
 
-    let presences = message.guild.presences.map(st => st.status);
+//     let presences = message.guild.presences.map(st => st.status);
 
-    let onlinemembers = 0;
-    let idlemembers = 0;
-    let dndmembers = 0;
+//     let onlinemembers = 0;
+//     let idlemembers = 0;
+//     let dndmembers = 0;
 
-    for(const i in presences) {
+//     for(const i in presences) {
 
-        if(presences[i] === "online") {
+//         if(presences[i] === "online") {
 
-            onlinemembers += 1;
+//             onlinemembers += 1;
 
-        }
+//         }
 
-    }
+//     }
 
-    for(const i in presences) {
+//     for(const i in presences) {
 
-        if(presences[i] === "idle") {
+//         if(presences[i] === "idle") {
 
-            idlemembers += 1;
+//             idlemembers += 1;
 
-        }
+//         }
 
-    }
+//     }
 
-    for(const i in presences) {
+//     for(const i in presences) {
 
-        if(presences[i] === "dnd") {
+//         if(presences[i] === "dnd") {
 
-            dndmembers += 1;
+//             dndmembers += 1;
 
-        }
+//         }
 
-    }
+//     }
 
-    let inviteEmbed = new Discord.RichEmbed()
-    .setColor(snow.blue)
-    .setDescription("SERVERCOUNT **" + snow.snowflake + "**")
-    .addField("MEMBERS", message.guild.memberCount, true)
-    .addField("ONLINE", onlinemembers + idlemembers + dndmembers, true)
-    .addField("HUMANS // BOTS", checkMembers(message.guild) + " **//** " + checkBots(message.guild))
-    .setFooter("SERVERCOUNT | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+//     let inviteEmbed = new Discord.RichEmbed()
+//     .setColor(snow.blue)
+//     .setDescription("SERVERCOUNT **" + snow.snowflake + "**")
+//     .addField("MEMBERS", message.guild.memberCount, true)
+//     .addField("ONLINE", onlinemembers + idlemembers + dndmembers, true)
+//     .addField("HUMANS // BOTS", checkMembers(message.guild) + " **//** " + checkBots(message.guild))
+//     .setFooter("SERVERCOUNT | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
     
-    message.channel.send(inviteEmbed);
+//     message.channel.send(inviteEmbed);
 
-}
+// }
 
-module.exports.help = {
-    name: "servercount"
-}
+// module.exports.help = {
+//     name: "servercount"
+// }
