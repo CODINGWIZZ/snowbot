8475f21aa9853571afed57dbc277b02c85f922d3
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-02 23:24:22 +0100

    REMOVEROLE

--- snowcommands/removerole.js
+++ snowcommands/removerole.js
@@ -10,25 +10,22 @@ module.exports.run = async (bot, message, args) => {
     let rrUser = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
     if(!rrUser) return message.channel.send("CAN'T FIND USER**!**");
 
-    let role = args.slice(1).join(" ");
-    if(!role) return message.channel.send("PLEASE SPECIFY A ROLE**!**");
-
-    let rrRole = message.guild.roles.find(`name`, role);
-    if(!rrRole) return message.channel.send("CAN'T FIND ROLE**!**");
+    let role = message.mentions.roles.first() || message.guild.roles.get(args.slice(1).join(" ")) || message.guild.roles.find(role => role.name === args.slice(1).join(" "));
+    if(!role) return message.channel.send("CAN'T FIND ROLE**!**");;
 
     if(rrUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT REMOVE A ROLE FROM A MEMBER WITH A HIGHER OR THE SAME ROLE AS YOU**!**");
-    if(!rrUser.roles.has(rrRole.id)) return message.channel.send("THAT USER DOESN'T HAVE THAT ROLE**!**");
+    if(!rrUser.roles.has(role.id)) return message.channel.send("THAT USER DOESN'T HAVE THAT ROLE**!**");
 
-    await(rrUser.removeRole(rrRole.id)).then(() => {
+    await(rrUser.removeRole(role.id)).then(() => {
 
-        message.channel.send(`<@${rrUser.id}> HAS BEEN REMOVED FROM THE **${rrRole}** ROLE**!**`);
+        message.channel.send(`<@${rrUser.id}> HAS BEEN REMOVED FROM THE **${role}** ROLE**!**`);
 
         let removeroleEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
         .setDescription("REMOVE ROLE **" + snow.snowflake + "**")
         .setTimestamp()
         .addField("USER", rrUser)
-        .addField("ROLE", "**" + rrRole + "**")
+        .addField("ROLE", "**" + role + "**")
         .addField("MODERATOR", message.author)
         .addField("CHANNEL", message.channel)
         .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);
