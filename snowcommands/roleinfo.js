f8cd0471c136d09e1be7ef4cbe0a8ebe4f25b98b
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-02 23:13:08 +0100

    ROLEINFO

--- snowcommands/roleinfo.js
+++ snowcommands/roleinfo.js
@@ -3,37 +3,36 @@ const snow = require("../snow.json");
 
 const convert = require("color-convert");
 
-module.exports.run = async(bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
 
-    let therole = args.join(" ");
-    if(!therole) return message.channel.send("PLEASE SPECIFY A ROLE YOU WANT TO CHECK INFORMATION ABOUT**!**");
-    
-    let role = message.guild.roles.find(`name`, therole);
+    if(!args[0]) return message.channel.send("PLEASE ENTER A ROLE YOU WANT TO GET INFO ABOUT**!**");
+
+    let role = message.mentions.roles.first() || message.guild.roles.get(args[0]) || message.guild.roles.find(role => role.name === args[0]);
     if(!role) return message.channel.send("CAN'T FIND ROLE**!**");
-    
+
+    let roleposition = message.guild.roles.size - role.calculatedPosition + " **/** " + message.guild.roles.size;
+
     let rolecolor = role.hexColor.replace("#", "");
     
     let mentionable = role.mentionable;
     let managed = role.managed;
     let hoist = role.hoist;
-    
+
     let truefalse = {
-        
         true: "TRUE",
         false: "FALSE"
-        
     };
-    
-    let roleinfoEmbed = new Discord.RichEmbed()
+
+    const roleinfoEmbed = new Discord.RichEmbed()
     .setColor(snow.blue)
-    .setDescription("ROLEINFO **" + snow.snowflake + "**\n**" + role + " (** " + role.id + " **)**")
-    .addField("POSITION", message.guild.roles.size - role.calculatedPosition + " **/** " + message.guild.roles.size, true)
+    .setDescription("ROLEINFO **" + snow.snowflake + "\n" + role.name + " (**" + role.id + " **)**")
+    .addField("POSITION", roleposition, true)
     .addField("ROLE MEMBERS", role.members.size, true)
-        .addField("CREATED AT", role.createdAt.toDateString().toUpperCase(), true)
+    .addField("CREATED AT", role.createdAt.toDateString().toUpperCase(), true)
     .addField("COLOR", "**#**" + rolecolor.toUpperCase() + "\n**RGB(**" + convert.hex.rgb(rolecolor) + "**)**", true)
     .addField("MENTIONABLE // MANAGED // HOIST", truefalse[mentionable] + " **//** " + truefalse[managed] + " **//** " + truefalse[hoist], true)
     .setFooter("ROLEINFO | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
-    
+
     message.channel.send(roleinfoEmbed);
 
 }
