f7f42b381aebfa4f38f37d55ee66928b7535e85f
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-05 23:31:10 +0100

    BAN

--- snowcommands/ban.js
+++ snowcommands/ban.js
@@ -1,40 +1,40 @@
-const Discord = require("discord.js");
-const snow = require("../snow.json");
+const Discord = require("discord.js");	
+const snow = require("../snow.json");	
 
-const fetch = require("snekfetch");
-const encode = require("strict-uri-encode");
+module.exports.run = async (bot, message, args) => {	
 
-module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
+    if(!args[0]) return message.channel.send("PLEASE MENTION A USER THAT YOU WANT TO BAN AND THEN A REASON**!**");	
 
-    let year = args[0];
-    if(!year) return message.channel.send("PLEASE ENTER THE YEAR AND THEN THE MOVIE SEARCH TERM**!**");
+    let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));	
+    if(!bUser) return message.channel.send("CAN'T FIND USER**!**");	
 
-    let movie = encode(args.slice(1).join(" "));
-    if(!movie) return message.channel.send("PLEASE ENTER A MOVIE SEARCH TERM**!**");
+    if(bUser.id === message.author.id) return message.channel.send("YOU CAN NOT BAN YOURSELF**!**");
+    if(bUser.hasPermission("MANAGE_MESSAGES")) return message.channel.send("THAT USER CAN'T BE BANNED**!**");	
+    let banreason = args.slice(1).join(" ");	
+    if(!banreason) return message.channek.send("PLEASE INCLUDE A REASON FOR THE BAN**!**");	
+    if(bUser.highestRole.position >= message.member.highestRole.position) return message.channel.send("YOU CAN NOT BAN A MEMBER WHO HAS A HIGHER OR THE SAME ROLE AS YOU**!**");
     
-    fetch.get(`http://omdbapi.com/?t=${movie}&y=${year}&apikey=65540ce6`).then((imdbmovie) => {
-        
-        let movieurl = `https://imdb.com/title/${imdbmovie.body.imdbID}`;
-    
-        let imdbEmbed = new Discord.RichEmbed()
-        .setColor(snow.blue)
-        .setThumbnail(imdbmovie.body.Poster)
-        .setURL(movieurl)
-        .setDescription("IMDB **" + snow.snowflake + "\n" + imdbmovie.body.Title + "\n`" + imdbmovie.body.imdbRating + "` /** `10` \n\n" + imdbmovie.body.Plot)
-        .addField("YEAR // RELEASED", imdbmovie.body.Year + " **//** " + imdbmovie.body.Released, true)
-        .addField("RUNTIME", imdbmovie.body.Runtime, true)
-        .addField("GENRE", imdbmovie.body.Genre)
-        .addField("DIRECTOR", imdbmovie.body.Director)
-        .addField("WRITER", imdbmovie.body.Writer)
-        .addField("ACTOR**(S)**", imdbmovie.body.Actors)
-        .setFooter("IMDB | SNOW " + snow.snowflake, bot.user.displayAvatarURL);
+    message.guild.member(bUser).ban(banreason);	
+    message.channel.send(`${bUser} HAS BEEN **BANNED** BY ${message.author} BECAUSE**: ${banreason}**`);	
 
-        message.channel.send(imdbEmbed);
-    
-    });
+    let banEmbed = new Discord.RichEmbed()	
+    .setColor(snow.blue)	
+    .setTimestamp()	
+    .setDescription("BAN **" + snow.snowflake + "**")	
+    .addField("USER", bUser)	
+    .addField("MODERATOR", message.author)	
+    .addField("CHANNEL", message.channel)	
+    .addField("REASON", banreason)	
+    .setFooter("SNOW " + snow.snowflake, bot.user.displayAvatarURL);	
 
+    let snowlog = message.guild.channels.find(`name`, "snow");	
+    if(!snowlog) return;	
+
+    snowlog.send(banEmbed);
+    
 }
 
-module.exports.help = {
-    name: "imdb"
+module.exports.help = {	
+    name: "ban"	
 }
