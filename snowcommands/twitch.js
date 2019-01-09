f395f4f9f65f1a527e512a4baf2c64f013cdbbf3
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-09 16:52:25 +0100

    TWITCH

--- snowcommands/twitch.js
+++ snowcommands/twitch.js
@@ -10,19 +10,19 @@ module.exports.run = async (bot, message, args) => {
     
     fetch.get(`https://api.twitch.tv/kraken/streams/${streamer}?client_id=${process.env.twitchclient}`).then((twitchstreamer) => {
         
-        let twitchstatus = twitchstreamer.body.stream.status;
-        let twitchurl = twitchstreamer.body.stream.url;
+        let twitchstatus = twitchstreamer.stream.body.status;
+        let twitchurl = twitchstreamer.stream.body.url;
         
-        let imageURL = `https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer.toLowerCase()}-640x360.jpg`;
+        let imageURL = `https://static-cdn.jtvnw.net/previews-ttv/live_user_${streamer.toLowerCase()}-320x180.jpg`;
     
         let twitchEmbed = new Discord.RichEmbed()
         .setColor("#6441a5")
-        .setAuthor(twitchstreamer.body.stream.display_name, twitchstreamer.body.stream.logo)
-        .setThumbnail(twitchstreamer.body.stream.logo)
-        .setURL(twitchstreamer.body.stream.url)
-        .setDescription(`[${twitchstatus}](${twitchurl})\n\n**GAME:** ${twitchstreamer.body.stream.game}`)
+        .setAuthor(twitchstreamer.stream.body.display_name, twitchstreamer.stream.body.logo)
+        .setThumbnail(twitchstreamer.stream.body.logo)
+        .setURL(twitchstreamer.stream.body.url)
+        .setDescription(`[${twitchstatus}](${twitchurl})\n\n**GAME:** ${twitchstreamer.stream.body.game}`)
         .setImage(imageURL)
-        .setFooter(`TOTAL VIEWS: ${twitchstreamer.body.stream.views} // FOLLOWERS: ${twitchstreamer.body.stream.followers}`, bot.user.displayAvatarURL);
+        .setFooter(`TOTAL VIEWS: ${twitchstreamer.stream.body.views} // FOLLOWERS: ${twitchstreamer.stream.body.followers}`, bot.user.displayAvatarURL);
         
         message.channel.send(twitchEmbed);
         
