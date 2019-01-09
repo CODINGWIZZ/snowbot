580ffca080df634469bc745d99af4059876e9295
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-09 07:14:30 +0100

    WEATHER

--- snowcommands/weather.js
+++ snowcommands/weather.js
@@ -12,8 +12,8 @@ module.exports.run = async (bot, message, args) => {
     let location = args.join(" ");
     if(!location) return message.channel.send("PLEASE ENTER A LOCATION OR A ZIP CODE THAT YOU WANT TO CHECK WEATHER ABOUT**!**");
 
-    let locationURL = location => `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22$%7B${encodeURIComponent(location)}%7D%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
-    let res = await got(locationURL(location), { json: true });
+//     let locationURL = location => `https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22$%7B${encodeURIComponent(location)}%7D%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`;
+//     let res = await got(locationURL(location), { json: true });
 
     let celsius = (fahrenheit) => Math.round(((fahrenheit - 32) * 5) / 9);
 
@@ -33,8 +33,8 @@ module.exports.run = async (bot, message, args) => {
             const countryinfo = countries.find(country => country.name === weatherinfo.location.country);
             const countryemoji = countryinfo ? countryinfo.emoji : "** **";
             
-            let sunrise = weatherinfo.astronomy.sunrise;
-            let sunset = weatherinfo.astronomy.sunset;
+//             let sunrise = weatherinfo.astronomy.sunrise;
+//             let sunset = weatherinfo.astronomy.sunset;
 
             let thecurrent = result[0].current;
             let thelocation = result[0].location;
@@ -47,8 +47,8 @@ module.exports.run = async (bot, message, args) => {
             .addField("FEELS LIKE", `${celsius(thecurrent.feelslike)}**` + "°C //** " + thecurrent.feelslike + "**°F**", true)
             .addField("WINDS", `*${thecurrent.winddisplay}* **>>** ${weatherinfo.wind.direction}**°**`, true)
             .addField("HUMIDITY", thecurrent.humidity + "**%**", true)
-            .addField("SUNRISE", sunrise, true)
-            .addField("SUNSET", sunset, true)
+//             .addField("SUNRISE", sunrise, true)
+//             .addField("SUNSET", sunset, true)
             .setFooter(`${thecurrent.observationpoint.toUpperCase()} | SNOW ❆`, bot.user.displayAvatarURL);
 
             weatherMessage.edit(weatherEmbed);
