1e50c7e3c512b5f9c395211fc182c79b06859ad3
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-13 22:59:03 +0100

    USERINFO

--- snowcommands/userinfo.js
+++ snowcommands/userinfo.js
@@ -23,7 +23,7 @@ module.exports.run = async (bot, message, args) => {
 
         };
         
-        let user = message.mentions.users.first() || message.guild.members.get(args[0]) || message.guild.members.get(member => member.user.username === args[0]) || message.author;
+        let user = message.mentions.users.first() || message.guild.members.get(member => member.user.username) || message.author;
         let member = message.guild.member(user);
         
         let roles = member.roles.array().slice(1).sort((a, b) => a.comparePositionTo(b)).reverse().map(role => role);  
