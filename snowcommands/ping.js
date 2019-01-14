7ba5f3c3b22a69ec7fca2cbc4ad2a0d9ea14e28e
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-14 15:56:56 +0100

    PING

--- snowcommands/ping.js
+++ snowcommands/ping.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
-let prefix = snow.prefix;
-
 module.exports.run = async (bot, message, args) => {
 
     message.channel.send("PINGING **...**").then((pingMessage) => {
