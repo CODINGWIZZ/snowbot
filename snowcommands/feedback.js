63178acbb77905fc72bc177ebcaef4a3af9b65c6
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-14 15:53:10 +0100

    FEEDBACK

--- snowcommands/feedback.js
+++ snowcommands/feedback.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
-let prefix = snow.prefix;
-
 module.exports.run = async (bot, message, args) => {
 
     let feedback = args.join(" ");
