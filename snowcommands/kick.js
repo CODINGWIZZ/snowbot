3f8e85b6d15e63c91b69771574954d65fd376c8c
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-14 15:55:25 +0100

    KICK

--- snowcommands/kick.js
+++ snowcommands/kick.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
-let prefix = snow.prefix;
-
 module.exports.run = async (bot, message, args) => {
 
     if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
