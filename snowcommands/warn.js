74b005e26f38202463b439974f99f588a32d50d5
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-14 16:03:48 +0100

    WARN

--- snowcommands/warn.js
+++ snowcommands/warn.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const snow = require("../snow.json");
 
-let prefix = snow.prefix;
-
 module.exports.run = async (bot, message, args) => {
 
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("YOU DO NOT HAVE PERMISSIONS TO DO THAT**!**");
