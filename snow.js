21126679aaa0b945cf58ff4c6c10961b7b156337
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-16 16:22:07 +0100

    SNOW

--- snow.js
+++ snow.js
@@ -269,7 +269,7 @@ bot.on("message", async message => {
         .setDescription("SNOW **" + snow.snowflake + "**")
         .addField("BOT NAME", "**" + snow.name + "**#" + bot.user.discriminator + " **(** " + bot.user.id + " **)**")
         .addField("DEVELOPER", "**" + snow.dev + "**#7897")
-        .addField("PREFIX // VERSION", "`s!` **//** " + snowpackage.version, true)
+        .addField("PREFIX // VERSION", "`s!` **//** " + snow.version, true)
         .addField("DISCORD VERSION", "`" + discordVersion + "`", true)
         .addField("NODE.JS VERSION", "`" + process.version.replace("v", "") + "`", true)
         .addField("MEMORY // COMMANDS", memoryusage + " **//** " + snow.commands)
