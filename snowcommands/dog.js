eaba0ecaf5ae62bb1f0434a4bd1df3c7742066c0
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-22 21:56:41 +0100

    DOG

--- snowcommands/dog.js
+++ snowcommands/dog.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
     let { body } = await superagent
     .get(`https://random.dog/woof.json`);
 
-    message.channel.send("GENERATING DOG IMAGE **...**").then((dogMessage) => {
+    message.channel.send("GENERATING DOG IMAGE**...**").then((dogMessage) => {
 
         let dogEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
