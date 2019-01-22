7600cbe4c99b96282c6ca03b1d61eb1fab0cb7f8
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-22 22:01:16 +0100

    FOX

--- snowcommands/fox.js
+++ snowcommands/fox.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     let randomfox = Math.floor(((Math.random()) * 120) + 1);
     let foxlink = `https://randomfox.ca/images/${randomfox}.jpg`;
     
-    message.channel.send("GENERATING FOX IMAGE **...**").then((foxMessage) => {
+    message.channel.send("GENERATING FOX IMAGE**...**").then((foxMessage) => {
         
         let foxEmbed = new Discord.RichEmbed()
         .setColor(snow.blue)
