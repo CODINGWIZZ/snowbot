2f7ee46ab266dcff36610c01b6ecc40653153020
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-23 22:31:37 +0100

    ROLEINFO

--- snowcommands/roleinfo.js
+++ snowcommands/roleinfo.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
 
     if(!args[0]) return message.channel.send("PLEASE ENTER A ROLE YOU WANT TO GET INFO ABOUT**!**");
 
-    let role = message.mentions.roles.first() || message.guild.roles.get(args[0]) || message.guild.roles.find(role.toUpperCase() => role.name.toUpperCase() === args[0]);
+    let role = message.mentions.roles.first() || message.guild.roles.get(args[0]) || message.guild.roles.find(role => role.name.toUpperCase() === args[0]);
     if(!role) return message.channel.send("CAN'T FIND ROLE**!**");
 
     let roleposition = message.guild.roles.size - role.calculatedPosition + " **/** " + message.guild.roles.size;
