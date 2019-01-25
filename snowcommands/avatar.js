f1ea14561e0b2f035b66eb959c1b0ede6478bb01
WIZZCODING <33423684+WIZZCODING@users.noreply.github.com>
2019-01-25 16:17:31 +0100

    AVATAR

--- snowcommands/avatar.js
+++ snowcommands/avatar.js
@@ -5,6 +5,17 @@ module.exports.run = async (bot, message, args) => {
 
     let avatar = message.mentions.users.first() || message.author;
     if(!avatar.avatarURL) return message.channel.send("CAN'T FIND THE AVATAR FOR THE DESIRED USER**!**");
+    
+    if(args[0] === "server") {
+     
+        let servericonEmbed = new Discord.RichEmbed()
+        .setColor(snow.blue)
+        .setDescription("AVATAR **" + snow.snowflake + " // " + message.guild.name + `**\n[**[ DOWNLOAD ]**](${message.guild.iconURL + "?size=2048"})`)
+        .setImage(message.guild.iconURL + "?size=2048")
+        
+        return message.channel.send(servericonEmbed);
+        
+    }
 
     let avatarEmbed = new Discord.RichEmbed()
     .setColor(snow.blue)
